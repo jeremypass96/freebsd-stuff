894aebcc71f4c66cc635d9835eadabf52f2168d9
Jeremy Passarelli <jeremy.pass.101@aol.com>
2021-02-21 21:39:43 -0500

    Removed doas.conf file, sudo is easier and is more commonly used. Also fixed a couple shell scrpits.

--- freebsd_symlinks.sh
+++ freebsd_symlinks.sh
@@ -2,8 +2,8 @@
 clear
 # Symlink bash executable to /bin/bash so that scripts written on/for Linux can run properly.
 echo "Making a Bash symlink to /bin/bash for Linux shell script compatability."
-doas ln -sv /usr/local/bin/bash /bin/bash
+sudo ln -sv /usr/local/bin/bash /bin/bash
 # Symlink themes/icons directories to enable installation of Linux themes.
 echo "Symlinking "/usr/local/share/themes" and "/usr/local/share/icons" to /usr/share/ for easy Linux themes and icons installation."
-doas ln -sv /usr/local/share/themes /usr/share/
-doas ln -sv /usr/local/share/icons /usr/share/
+sudo ln -sv /usr/local/share/themes /usr/share/
+sudo ln -sv /usr/local/share/icons /usr/share/
--- freebsd_themes_install.sh
+++ freebsd_themes_install.sh
@@ -1,24 +1,22 @@
 #!/usr/local/bin/bash
-
 cd ~
 # Install the Vertex GTK theme.
-doas pkg install gnome-themes-extra gtk-murrine-engine autoconf automake pkgconf gtk3 git
+sudo pkg install gnome-themes-extra gtk-murrine-engine autoconf automake pkgconf gtk3
 git clone https://github.com/horst3180/vertex-theme --depth 1 && cd vertex-theme
-./autogen.sh --prefix=/usr --disable-cinnamon --disable-gnome-shell --disable-light -disable-unity --disable-xfwm --with-gnome=3.22
-doas make install
+./autogen.sh --prefix=/usr --disable-gnome-shell --disable-light --disable-unity --disable-xfwm --with-gnome=3.22
+sudo make install
 cd ~
 rm -rf vertex-theme/
-# Install the Arc GTK theme.
-doas pkg install gtk-arc-themes
+# Install the Arc and Materia GTK theme.
+sudo pkg install gtk-arc-themes materia-gtk-theme
 # Install the Vimix GTK theme. Installing only for window borders.
 git clone https://github.com/vinceliuice/vimix-gtk-themes.git && cd vimix-gtk-themes/
-doas ./install.sh -d /usr/share/themes -s standard
+sudo ./install.sh -d /usr/share/themes -s standard
 cd ~
 rm -rf vimix-gtk-themes/
 # Install Kvantum Qt5 theme manager.
-doas pkg install Kvantum-qt5
-# Install Qogir cursor theme.
-git clone https://github.com/vinceliuice/Qogir-icon-theme.git && cd Qogir-icon-theme/src/cursors/
-doas ./install.sh
-cd ~
-rm -rf Qogir-icon-theme/
+sudo pkg install Kvantum-qt5
+# Install Qogir icon theme.
+wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MTI5MjY2OTMiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6Ijg4ZGIyNjk0OTljZmIxZGI3MWE0Mjc1YWMzYTQwN2E3NmExMGVjZWRmYTI4NTExOWJjZDg5NDZmODNkYTJjZDc2ZjZiMTA2YmZlOGUwYTYzOTIxOTZlMTU4MWZmYzJlNWI4YzQ5YWRiNWU2MTFkZDc5ZWIyZjVhNTRkY2UwZDIxIiwidCI6MTYxMzk2MjA5OSwic3RmcCI6ImE1YmIwZTllY2Y4NWNiMmI2MTgxYmEyOGY5NTAxODU0Iiwic3RpcCI6IjE3NC44NC41Ny4xMSJ9.TBX12Il6NpH8Hv4vYVGhS-T6g1X3OwFcn7wFoAX7cs4/Qogir-ubuntu.tar.xz
+tar -xvf Qogir-ubuntu.tar.xz
+cp -rv Qogir-* /usr/local/share/themes
