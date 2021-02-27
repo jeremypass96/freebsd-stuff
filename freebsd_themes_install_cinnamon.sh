48c1251ba9670608b1c2425cb8b2cc7493a2fe55
Jeremy Passarelli <jeremy.pass.101@aol.com>
2021-02-27 00:39:11 -0500

    Fixed Cinnamon theme install script.

--- freebsd_themes_install_cinnamon.sh
+++ freebsd_themes_install_cinnamon.sh
@@ -11,6 +11,7 @@ rm -rf vertex-theme/
 # Install Kvantum Qt5 theme manager.
 sudo pkg install -y Kvantum-qt5
 # Install Qogir icon theme.
+sudo pkg install -y wget
 wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MTI5MjY2OTMiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6Ijg4ZGIyNjk0OTljZmIxZGI3MWE0Mjc1YWMzYTQwN2E3NmExMGVjZWRmYTI4NTExOWJjZDg5NDZmODNkYTJjZDc2ZjZiMTA2YmZlOGUwYTYzOTIxOTZlMTU4MWZmYzJlNWI4YzQ5YWRiNWU2MTFkZDc5ZWIyZjVhNTRkY2UwZDIxIiwidCI6MTYxMzk2MjA5OSwic3RmcCI6ImE1YmIwZTllY2Y4NWNiMmI2MTgxYmEyOGY5NTAxODU0Iiwic3RpcCI6IjE3NC44NC41Ny4xMSJ9.TBX12Il6NpH8Hv4vYVGhS-T6g1X3OwFcn7wFoAX7cs4/Qogir-ubuntu.tar.xz
 tar -xvf Qogir-ubuntu.tar.xz
 cp -rv Qogir-ubuntu /usr/share/themes
