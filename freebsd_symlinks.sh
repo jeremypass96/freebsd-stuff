3eabdb5babcbe454f2edf1a3cca9b3a3471767fd
Jeremy Passarelli <jeremy.pass.101@aol.com>
2021-02-27 00:41:02 -0500

    Oops! Fixed symlinks script.

--- freebsd_symlinks.sh
+++ freebsd_symlinks.sh
@@ -5,5 +5,5 @@ echo "Making a bash symlink to /bin/bash for Linux shell script compatability...
 sudo ln -sv /usr/local/bin/bash /bin/bash
 # Symlink themes/icons directories to enable installation of Linux themes.
 echo "Symlinking "/usr/local/share/themes" and "/usr/local/share/icons" to /usr/share/ for easy Linux themes and icons installation..."
-sudo ln -sv /usr/local/share/themes /usr/share/
-sudo ln -sv /usr/local/share/icons /usr/share/
+sudo ln -sv /usr/local/share/themes /usr/share
+sudo ln -sv /usr/local/share/icons /usr/share
