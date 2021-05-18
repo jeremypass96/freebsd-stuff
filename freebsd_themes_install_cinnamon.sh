257f77fc1b253a45684f5241e0b504cd649650ed
Jeremy Passarelli <jeremy.pass.101@aol.com>
2021-05-18 10:11:47 -0400

    Modified themes to be installed. Cinnamon gets the Yaru icons/GTK theme, and MATE gets the Canta GTK theme with the Numix circle icon theme.

--- freebsd_themes_install_cinnamon.sh
+++ freebsd_themes_install_cinnamon.sh
@@ -1,6 +1,9 @@
 #!/usr/local/bin/bash
 cd
+#
+clear
 # Install the Vertex GTK theme.
+echo "Installing the Vertex GTK theme (for LightDM login screen)..."
 sudo pkg install -y gnome-themes-extra gtk-murrine-engine autoconf automake pkgconf gtk3
 git clone https://github.com/horst3180/vertex-theme --depth 1 && cd vertex-theme
 ./autogen.sh --prefix=/usr --disable-gnome-shell --disable-light --disable-unity --disable-xfwm --with-gnome=3.22
@@ -8,32 +11,33 @@ sudo make install
 cd
 rm -rf vertex-theme/
 #
+clear
 # Install Kvantum Qt5 theme manager.
+echo "Installing the Kvantum Qt5 theme manager..."
 sudo pkg install -y Kvantum-qt5
-# Install Qogir icon theme.
-sudo pkg install -y wget
-wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MTI5MjY2OTMiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6Ijg4ZGIyNjk0OTljZmIxZGI3MWE0Mjc1YWMzYTQwN2E3NmExMGVjZWRmYTI4NTExOWJjZDg5NDZmODNkYTJjZDc2ZjZiMTA2YmZlOGUwYTYzOTIxOTZlMTU4MWZmYzJlNWI4YzQ5YWRiNWU2MTFkZDc5ZWIyZjVhNTRkY2UwZDIxIiwidCI6MTYxMzk2MjA5OSwic3RmcCI6ImE1YmIwZTllY2Y4NWNiMmI2MTgxYmEyOGY5NTAxODU0Iiwic3RpcCI6IjE3NC44NC41Ny4xMSJ9.TBX12Il6NpH8Hv4vYVGhS-T6g1X3OwFcn7wFoAX7cs4/Qogir-ubuntu.tar.xz
-tar -xvf Qogir-ubuntu.tar.xz
-cp -rv Qogir-ubuntu /usr/share/themes
-cp -rv Qogir-ubuntu-dark /usr/share/themes
-rm Qogir-ubuntu.tar.xz
 #
-# Install the Yaru GTK/sound theme.
-sudo pkg install -y meson sassc python3
-git clone https://github.com/ubuntu/yaru.git
-echo "option('icons', type: 'boolean', value: false, description:'build icons component')" > meson_options.txt
-echo "option('gnome-shell', type: 'boolean', value: false, description:'build gnome-shell component')" >> meson_options.txt
-echo "option('gnome-shell-gresource', type: 'boolean', value: false, description: 'build gnome-shell component in gresources')" >> meson_options.txt
-echo "option('gtk', type: 'boolean', value: true, description:'build gtk component')" >> meson_options.txt
-echo "option('gtksourceview', type: 'boolean', value: false, description:'build gtksourceview component')" >> meson_options.txt
-echo "option('sounds', type: 'boolean', value: true, description:'build sounds component')" >> meson_options.txt
-echo "option('sessions', type: 'boolean', value: false, description:'build sessions component')" >> meson_options.txt
-echo "option('communitheme_compat', type: 'boolean', value: false, description:'build communitheme-compact')" >> meson_options.txt
-echo "" >> meson_options.txt
-echo "option('default', type: 'boolean', value: true, description:'build Yaru gtk default flavour')" >> meson_options.txt
-echo "option('dark', type: 'boolean', value: false, description:'build Yaru gtk dark flavour')" >> meson_options.txt
-echo "option('light', type: 'boolean', value: false, description:'build Yaru gtk light flavour')" >> meson_options.txt
-echo "option('ubuntu-unity', type: 'boolean', value: false, description:'build Yaru with Unity assets')" >> meson_options.txt
-sudo ninja -C "build" install
+clear
+# Install Qogir mouse cursors.
+echo "Installing Qogir mouse cursors..."
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git && cd Qogir-icon-theme/src/cursors/
+sudo ./install.sh
+cd
+rm -rf Qogir-icon-theme/
+#
+clear
+# Install the Vimix GTK theme (for window borders ony).
+echo "Installing Vimix GTK theme (for window borders only)..."
+git clone https://github.com/vinceliuice/vimix-gtk-themes.git && cd vimix-gtk-themes/
+sudo ./install.sh -d /usr/share/themes -s standard
+cd
+rm -rf vimix-gtk-themes/
+#
+clear
+# Install the Yaru GTK/icon theme.
+echo "Installing the Yaru GTK/icon theme..."
+sudo pkg install -y yaru-gtk-themes yaru-icon-theme
+#
+clear
 # Install fonts.
+echo "Installing fonts..."
 sudo pkg install -y ubuntu-font sourcecodepro-ttf
--- freebsd_themes_install_mate.sh
+++ freebsd_themes_install_mate.sh
@@ -1,29 +1,32 @@
 #!/usr/local/bin/bash
 cd
-# Install the Arc and Materia GTK theme.
-echo "Installing the Arc GTK theme..."
-sudo pkg install -y gtk-arc-themes
 #
+clear
+# Install the Vertex GTK theme.
+echo "Installing the Vertex GTK theme (for LightDM login screen)..."
+sudo pkg install -y gnome-themes-extra gtk-murrine-engine autoconf automake pkgconf gtk3
+git clone https://github.com/horst3180/vertex-theme --depth 1 && cd vertex-theme
+./autogen.sh --prefix=/usr --disable-gnome-shell --disable-light --disable-unity --disable-xfwm --with-gnome=3.22
+sudo make install
+cd
+rm -rf vertex-theme/
+#
+clear
+# Install the Canta GTK theme.
+echo "Installing the Canta GTK theme..."
+sudo pkg install -y canta-gtk-themes
+#
+clear
 # Install Kvantum Qt5 theme manager.
 echo "Installing the Kvantun Qt5 theme manager..."
 sudo pkg install -y Kvantum-qt5
 #
-# Install Tango icon theme and fix the menu logo icon.
-echo "Fixing stupid MATE menu icon. Replacing the Tango icon logo with the FreeBSD logo..."
-sudo pkg install -y icons-tango icons-tango-extras svg2png gtk-update-icon-cache
-git clone https://github.com/cbrnix/Newaita.git
-cd Newaita/Newaita/.places-default/24/
-svg2png freebsd.svg freebsd.png
-sudo cp -v freebsd.png /usr/local/share/icons/Tango/22x22/places/start-here.png
-sudo cp -v freebsd.png /usr/local/share/icons/Tango/24x24/places/start-here.png
-sudo cp -v freebsd.png /usr/local/share/icons/Tango/32x32/places/start-here.png
-sudo cp -v freebsd.svg /usr/local/share/icons/Tango/scalable/places/start-here.svg
-sudo gtk-update-icon-cache /usr/local/share/icons/Tango/
-cd
-echo "Icon is now fixed!"
-echo "Removing source icon folders..."
-rm -rf Newaita/ && rm -rf /usr/local/share/icons/Newaita
+clear
+# Install Numix circle icon theme.
+echo "Installing the Numix circle icon theme..."
+sudo pkg install -y numix-icon-theme-circle numix-icon-theme
 #
+clear
 # Install Qogir mouse cursors.
 echo "Installing Qogir mouse cursors..."
 git clone https://github.com/vinceliuice/Qogir-icon-theme.git && cd Qogir-icon-theme/src/cursors/
@@ -31,11 +34,15 @@ sudo ./install.sh
 cd
 rm -rf Qogir-icon-theme/
 #
+clear
 # Install the Vimix GTK theme (for window borders ony).
 echo "Installing Vimix GTK theme (for window borders only)..."
 git clone https://github.com/vinceliuice/vimix-gtk-themes.git && cd vimix-gtk-themes/
 sudo ./install.sh -d /usr/share/themes -s standard
 cd
 rm -rf vimix-gtk-themes/
+#
+clear
 # Installing fonts.
+echo "Installing fonts..."
 sudo pkg install -y ubuntu-font sourcecodepro-ttf
