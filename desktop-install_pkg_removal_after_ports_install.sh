03e71ba8322479d15374c1fa38dcfaf853c6ab14
Jeremy Passarelli <jeremy.pass.101@aol.com>
2021-06-03 21:13:47 -0400

    Xorg session manager *MIGHT* be needed, so keeping installed.

--- desktop-install_pkg_removal_after_ports_install.sh
+++ desktop-install_pkg_removal_after_ports_install.sh
@@ -1,2 +1,2 @@
 #!/usr/local/bin/bash
-sudo pkg remove -y xdm xsm hplip xf86-video-mga xf86-video-vesa && sudo pkg autoremove -y
+sudo pkg remove -y xdm hplip xf86-video-mga xf86-video-vesa && sudo pkg autoremove -y
