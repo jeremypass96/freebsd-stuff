39c1e7d065a80192f530320f647fd5b8bb1c7a21
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-08-04 01:12:01 -0400

    Update freebsd_themes_install_mate.sh

--- freebsd_themes_install_mate.sh
+++ freebsd_themes_install_mate.sh
@@ -2,15 +2,6 @@
 cd
 #
 clear
-# Install the Vertex GTK theme.
-echo "Installing the Vertex GTK theme (for LightDM login screen)..."
-sudo pkg install -y gnome-themes-extra gtk-murrine-engine autoconf automake pkgconf gtk3
-git clone https://github.com/horst3180/vertex-theme --depth 1 && cd vertex-theme
-./autogen.sh --prefix=/usr/local --disable-gnome-shell --disable-light --disable-unity --disable-xfwm --with-gnome=3.22
-sudo make install
-cd && rm -rf vertex-theme/
-#
-clear
 # Install the Greybird GTK theme.
 echo "Installing the Greybird GTK theme..."
 sudo pkg install -y greybird-theme
@@ -62,8 +53,7 @@ clear
 echo "Installing Qogir mouse cursors..."
 git clone https://github.com/vinceliuice/Qogir-icon-theme.git && cd Qogir-icon-theme/src/cursors/
 sudo ./install.sh
-cd
-rm -rf Qogir-icon-theme/
+cd && rm -rf Qogir-icon-theme/
 #
 clear
 # Install the Mojave GTK theme (for window borders ony).
@@ -73,4 +63,4 @@ sudo pkg install -y mojave-gtk-themes
 clear
 # Installing fonts.
 echo "Installing fonts..."
-sudo pkg install -y ubuntu-font sourcecodepro-ttf roboto-fonts-ttf
+sudo pkg install -y ubuntu-font sourcecodepro-ttf
