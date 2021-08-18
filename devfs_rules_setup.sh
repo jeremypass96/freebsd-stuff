b5cb56d2bdb7aa0bc01ad7d61c4f70f7f96e0a50
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-08-18 00:49:56 -0400

    Add files via upload

---
+++ devfs_rules_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Sets up a devfs.rules file on FreeBSD for FreeBSD desktop use. Run this script as root!
+echo "[devfsrules_common=7]" > /etc/devfs.rules
+echo "add path 'ad*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'da*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'acd*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'cd*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'mmcsd*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'pass*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'xpt*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'ugen*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'usbctl' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'usb*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'lpt*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'ulpt*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'unlpt*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'fd*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'uscan*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'video*' mode 0666 group operator" >> /etc/devfs.rules
+echo "add path 'dvb/*' mode 0666 group operator" >> /etc/devfs.rules
+echo 'devfs_system_ruleset="devfsrules_common"' >> /etc/rc.conf
+# Install automount via ports.
+cd /usr/ports/sysutils/automount
+make && make install clean ; cd
+service devd restart
