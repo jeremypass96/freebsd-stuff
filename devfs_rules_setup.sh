45ff28f1797f6cc739cde3fc9dd9bf137812d4a1
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-09-09 18:51:35 -0400

    Update and rename devfs_rules_setup_ports.sh to devfs_rules_setup.sh

--- devfs_rules_setup.sh
+++ devfs_rules_setup.sh
@@ -20,7 +20,6 @@ echo "add path 'video*' mode 0666 group operator" >> /etc/devfs.rules
 echo "add path 'dvb/*' mode 0666 group operator" >> /etc/devfs.rules
 echo 'devfs_system_ruleset="devfsrules_common"' >> /etc/rc.conf
 # Install automount via ports.
-cd /usr/ports/sysutils/automount
-make install clean ; cd
+pkg install automount
 service devd enable
 service devd restart
