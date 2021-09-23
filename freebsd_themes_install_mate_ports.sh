4a4795dbabab77e5dc24928d1710efbbea3d335f
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-09-23 15:46:22 -0400

    Update freebsd_themes_install_mate_ports.sh

--- freebsd_themes_install_mate_ports.sh
+++ freebsd_themes_install_mate_ports.sh
@@ -17,30 +17,9 @@ sudo cp -rv Greybird /usr/local/share/Kvantum/
 cd && rm -rf greybird-kde
 #
 clear
-# Install Faenza icon theme with the FreeBSD logo from the Newaita icon set for Applications menu. Not working for some reason.
-echo "Installing the Faenza icon theme with the FreeBSD logo from the Newaita icon set for Applications menu..."
-sudo portmaster graphics/svg2png
-sudo portmaster x11-themes/gnome-icons-faenza
-cd /usr/local/share/icons/Faenza
-cd places/24 && sudo rm -rf start-here.png
-sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/24/freebsd.svg -o start-here-freebsd.svg
-sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
-sudo ln -s start-here-freebsd.png start-here.png
-cd ../32 && sudo rm -rf start-here.png
-sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/32/freebsd.svg -o start-here-freebsd.svg
-sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
-sudo ln -s start-here-freebsd.png start-here.png
-cd ../48 && sudo rm -rf start-here.png
-sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/48/freebsd.svg -o start-here-freebsd.svg
-sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
-sudo ln -s start-here-freebsd.png start-here.png
-cd ../64 && sudo rm -rf start-here.png
-sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/64/freebsd.svg -o start-here-freebsd.svg
-sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
-sudo ln -s start-here-freebsd.png start-here.png
-cd ../scalable && sudo rm -rf start-here.svg
-sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/64/freebsd.svg -o start-here-freebsd.svg
-sudo ln -s start-here-freebsd.svg start-here.svg
+# Install Faenza icon theme (MATE version).
+echo "Installing the Faenza icon theme (MATE version)..."
+sudo portmaster x11-themes/mate-icon-theme-faenza
 #
 clear
 # Install Qogir mouse cursors.
@@ -62,7 +41,7 @@ sudo portmaster x11-fonts/sourcecodepro-ttf
 sudo portmaster x11-fonts/webfonts
 #
 # Install the Vertex GTK theme.
-echo "Installing the Vertex GTK theme (for LightDM login screen)..."
+echo "Installing the Vertex GTK theme (for LightDM *GTK* login screen)..."
 sudo portmaster x11-themes/gnome-themes-extra x11-themes/gtk-murrine-engine devel/autoconf devel/automake devel/pkgconf x11-toolkits/tk30
 git clone https://github.com/horst3180/vertex-theme --depth 1 && cd vertex-theme
 ./autogen.sh --prefix=/usr/local --disable-gnome-shell --disable-light --disable-unity --disable-xfwm --with-gnome=3.22
