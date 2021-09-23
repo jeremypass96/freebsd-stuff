06840a815f0aa99d74f781d4935d48e356a4fbcf
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-09-23 00:55:55 -0400

    Update freebsd_themes_install_mate_ports.sh

--- freebsd_themes_install_mate_ports.sh
+++ freebsd_themes_install_mate_ports.sh
@@ -17,37 +17,30 @@ sudo cp -rv Greybird /usr/local/share/Kvantum/
 cd && rm -rf greybird-kde
 #
 clear
-# Install Newaita icon theme with FreeBSD logo for Applications menu.
-echo "Installing the Newaita icon theme with FreeBSD logo for Applications menu..."
-cd
-git clone https://github.com/cbrnix/Newaita.git
-cd Newaita/
-sudo cp -rv Newaita /usr/local/share/icons/
-cd /usr/local/share/icons/Newaita/
-clear
-## Change "rm -f" to "rm -rf"
-echo "Changing "rm -f" to "rm -rf" first before we can execute."
-sudo micro FV.sh
-##
-sudo ./FV.sh
-## Change "rm -f" to "rm -rf"
-echo "Changing "rm -f" to "rm -rf" first before we can execute."
-sudo micro PV.sh
-##
-sudo ./PV.sh
-cd places/24
-sudo rm -rf start-here.svg
-sudo ln -s distributor-logo-freebsd.svg start-here.svg
-cd ../32
-sudo rm -rf start-here.svg
-sudo ln -s distributor-logo-freebsd.svg start-here.svg
-cd ../48
-sudo rm -rf start-here.svg
-sudo ln -s distributor-logo-freebsd.svg start-here.svg
-cd ../64
-sudo rm -rf start-here.svg
-sudo ln -s distributor-logo-freebsd.svg start-here.svg
-cd && rm -rf Newaita
+# Install Faenza icon theme with the FreeBSD logo from the Newaita icon set for Applications menu. Not working for some reason.
+echo "Installing the Faenza icon theme with the FreeBSD logo from the Newaita icon set for Applications menu..."
+sudo portmaster graphics/svg2png
+sudo portmaster x11-themes/gnome-icons-faenza
+cd /usr/local/share/icons/Faenza
+cd places/24 && sudo rm -rf start-here.png
+sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/24/freebsd.svg -o start-here-freebsd.svg
+sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
+sudo ln -s start-here-freebsd.png start-here.png
+cd ../32 && sudo rm -rf start-here.png
+sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/32/freebsd.svg -o start-here-freebsd.svg
+sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
+sudo ln -s start-here-freebsd.png start-here.png
+cd ../48 && sudo rm -rf start-here.png
+sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/48/freebsd.svg -o start-here-freebsd.svg
+sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
+sudo ln -s start-here-freebsd.png start-here.png
+cd ../64 && sudo rm -rf start-here.png
+sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/64/freebsd.svg -o start-here-freebsd.svg
+sudo svg2png start-here-freebsd.svg start-here-freebsd.png && sudo rm -rf start-here-freebsd.svg
+sudo ln -s start-here-freebsd.png start-here.png
+cd ../scalable && sudo rm -rf start-here.svg
+sudo fetch https://raw.githubusercontent.com/cbrnix/Newaita/master/Newaita/.places-default/64/freebsd.svg -o start-here-freebsd.svg
+sudo ln -s start-here-freebsd.svg start-here.svg
 #
 clear
 # Install Qogir mouse cursors.
