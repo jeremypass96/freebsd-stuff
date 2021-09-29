92beae798b7240059a8ad897535b345c88fd8791
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-09-29 13:23:48 -0400

    Update freebsd_themes_install_mate_ports.sh

--- freebsd_themes_install_mate_ports.sh
+++ freebsd_themes_install_mate_ports.sh
@@ -14,11 +14,3 @@ clear
 # Installing fonts.
 echo "Installing fonts..."
 sudo portmaster x11-fonts/ubuntu-font x11-fonts/office-code-pro x11-fonts/webfonts x11-fonts/droid-fonts-ttf x11-fonts/materialdesign-ttf
-#
-# Install the Vertex GTK theme.
-echo "Installing the Vertex GTK theme (for LightDM login screen)..."
-sudo portmaster x11-themes/gnome-themes-extra x11-themes/gtk-murrine-engine devel/autoconf devel/automake devel/pkgconf
-git clone https://github.com/horst3180/vertex-theme --depth 1 && cd vertex-theme
-./autogen.sh --prefix=/usr/local --disable-gnome-shell --disable-light --disable-unity --disable-xfwm --with-gnome=3.22
-sudo make install clean
-cd && rm -rf vertex-theme
