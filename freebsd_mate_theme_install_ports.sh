c0c29553673fee03a78b2ad12a67ff4432e55219
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-10-15 17:41:17 -0400

    Update freebsd_mate_theme_install_ports.sh

--- freebsd_mate_theme_install_ports.sh
+++ freebsd_mate_theme_install_ports.sh
@@ -33,8 +33,9 @@ cd && rm -rf macOSBigSur.tar.gz
 # Get extra wallpapers!
 echo "Getting extra wallpapers..."
 sudo git clone https://gitlab.com/dwt1/wallpapers.git
-cd wallpapers/ && cp -v *.jpg /usr/local/share/backgrounds/
+cd wallpapers && mv -v *.jpg /usr/local/share/backgrounds/
 cd /usr/local/share/backgrounds && sudo chown root:wheel *.jpg && cd
+rm -rf wallpapers
 clear
 # Set up common folders in users home directory.
 cd /usr/ports/devel/xdg-user-dirs && sudo make install clean
