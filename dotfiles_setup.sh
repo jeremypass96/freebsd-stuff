d6a86f4d3ca3caa00825c8dc4638ba45ab181bc7
Jeremy Passarelli <jeremy.pass.101@aol.com>
2021-10-15 00:20:43 -0400

    Made dotfiles setup file executable and fixed an error that I miseed in that file.

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -2,7 +2,7 @@
 cd Dotfiles/
 
 # Copy over .profile & fix system profile file.
-cp -v .profile ~/
+cp -v .profile ~
 sudo cp -v .profile /usr/share/skel/dot.profile
 sudo echo "EDITOR=micro;    export EDITOR" >> /etc/profile
 sudo echo "PAGER=less;   export PAGER" >> /etc/profile
@@ -10,12 +10,12 @@ sudo echo "MANPAGER=less;    export MANPAGER" >> /etc/profile
 sudo echo "QT_QPA_PLATFORMTHEME=qt5ct;   export QT_QPA_PLATFORMTHEME" >> /etc/profile
 
 # Copy over zsh config
-cp -v .zshrc ~/
+cp -v .zshrc ~
 sudo cp -v .zshrc /usr/share/skel/dot.zshrc
 
 # Copy over neofetch config
 mkdir -p ~/.config/neofetch
-cp config/neofetch/neofetch.conf ~/.config/neofetch/
+cp -v config/neofetch/config.conf ~/.config/neofetch/
 
 # Change shell to zsh
 chsh -s /usr/local/bin/zsh $USER
