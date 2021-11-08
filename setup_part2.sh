4cb09e05640611ad8024d8e41b67eae64ae0e622
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-11-08 16:53:47 -0500

    Update setup_part2.sh

--- setup_part2.sh
+++ setup_part2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is the second part of the FreeBSD setup script. Do NOT run as root!
-echo "Are you installing software via ports or packages?"
+echo "Are you installing software via ports or packages? (ports/pkg)"
 read answer
 if [ $answer = "ports" ] ; then
 ./freebsd_mate_theme_install_ports.sh
