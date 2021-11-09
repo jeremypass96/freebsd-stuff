274a74813b467a0c85e3f65dee995db0eba92d6e
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-11-09 13:23:34 -0500

    Update rcconf_setup_ports.sh
    Enabled APM BIOS functions.

--- rcconf_setup_ports.sh
+++ rcconf_setup_ports.sh
@@ -33,6 +33,7 @@ sysrc virecover_enable="NO"
 sysrc vboxnet_enable="YES"
 sysrc smartd_enable="YES"
 sysrc dumpdev="NO"
+sysrc apm_enable="YES"
 service ntpdate delete
 # Set up DRM kmod support for graphics cards.
 cd /usr/ports/graphics/drm-kmod && make install clean
