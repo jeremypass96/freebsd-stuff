c9046180c0e2a0553c3f1199531c1b24ba677105
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-11-16 10:26:10 -0500

    Update bootloader_setup.sh

--- bootloader_setup.sh
+++ bootloader_setup.sh
@@ -3,7 +3,7 @@
 
 # Checking to see if we're running as root.
 if [ $(id -u) -ne 0 ] ; then
-echo "Please run this setup script as root! Thanks."
+echo "Please run this bootloader setup script as root! Thanks."
 exit
 fi
 
