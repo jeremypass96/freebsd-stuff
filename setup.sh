79c10c03805041b1c0d4d34d9e9531640d8458c4
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-11-16 10:20:09 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
 # This script will set up a complete FreeBSD desktop for you, ready to go when you reboot.
+
+# Checking to see if we're running as root.
 if [ $(id -u) -ne 0 ] ; then
 echo "Please run this setup script as root via 'su'! Thanks."
 exit
 fi
-#
+
 clear
+
 echo "Welcome to the FreeBSD post-install setup script. This script will setup Xorg, MATE, some useful software for you, along with system files being tweaked for desktop use."
 echo "Do you plan to install software via pkg (binary packages) or ports? (pkg/ports)"
 read answer
 if [ $answer = "pkg" ] ; then
+
 # Update repo to use latest packages.
 mkdir -p /usr/local/etc/pkg/repos
 cat << EOF >/usr/local/etc/pkg/repos/FreeBSD.conf
 FreeBSD: { url: "http://pkg0.nyi.freebsd.org/${ABI}/latest" }
 EOF
 pkg update
+
 # Install packages.
 pkg install -y sudo xorg-minimal xorg-drivers xorg-fonts xorg-libraries noto-basic noto-emoji cups papersize-default-letter mate xfburn parole firefox thunderbird audacity handbrake isomaster abiword gnumeric transmission-gtk asunder gimp inkscape pinta shotwell webfonts virtualbox-ose micro zsh ohmyzsh neofetch lightdm slick-greeter mp4v2 classiclooks flatery-icon-themes i386-wine wine-mono wine-gecko numlockx devcpu-data automount unix2dos
 ./rcconf_setup.sh
 cp /usr/local/etc/smartd.conf.sample /usr/local/etc/smartd.conf
 echo "/dev/ada0 -H -l error -f" >> /usr/local/etc/smartd.conf
 fi
-#
+
 if [ $answer = "ports" ] ; then
+
+# Copying over make.conf file. 
 cp -v make.conf /etc/
+
+# Avoid pulling in Ports tree categories with non-English languages.
 sed -i '' s/#REFUSE arabic chinese french german hebrew hungarian japanese/REFUSE arabic chinese french german hebrew hungarian japanese/g /etc/portsnap.conf
 sed -i '' s/#REFUSE korean polish portuguese russian ukrainian vietnamese/REFUSE korean polish portuguese russian ukrainian vietnamese/g /etc/portsnap.conf
+
+# Pull in Ports tree, extract, and update it.
 portsnap auto
+
+# Install Ports.
 cd /usr/ports/security/sudo && make install clean
 cd /usr/ports/editors/micro && make install clean
 cd /usr/ports/shells/zsh && make install clean
@@ -67,18 +80,24 @@ cd /usr/ports/x11/numlockx && make install clean
 cd /usr/ports/sysutils/devcpu-data && make install clean
 cd /usr/ports/sysutils/automount && make install clean
 cd /usr/ports/converters/unix2dos/ && make install clean
+
+# Setup rc.conf file.
 ./rcconf_setup_ports.sh
 fi
+
+# Setup system files for desktop use.
 ./sysctl_setup.sh
 ./bootloader_setup.sh
 ./devfs_setup.sh
+
 # Setup automoumt.
 cat << EOF >/usr/local/etc/automount.conf
 USERUMOUNT=YES
 REMOVEDIRS=YES
 ATIME=NO
 EOF
-# Setup LightDM/Slick Greeter.
+
+# Setup LightDM.
 sed -i '' s/#pam-autologin-service=lightdm-autologin/pam-autologin-service=lightdm-autologin/g /usr/local/etc/lightdm/lightdm.conf
 sed -i '' s/#greeter-session=example-gtk-gnome/greeter-session=slick-greeter/g /usr/local/etc/lightdm/lightdm.conf
 sed -i '' s/#allow-user-switching=true/allow-user-switching=true/g /usr/local/etc/lightdm/lightdm.conf
@@ -89,6 +108,8 @@ sed -i '' s/#autologin-user-timeout=0/autologin-user-timeout=0/g /usr/local/etc/
 mkdir /usr/local/etc/lightdm/wallpaper
 fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0062.jpg\?inline\=false -o /usr/local/etc/lightdm/wallpaper/0062.jpg
 sudo chown root:wheel /usr/local/etc/lightdm/wallpaper/0062.jpg
+
+# Setup slick greeter.
 cat << EOF >/usr/local/etc/lightdm/slick-greeter.conf
 [Greeter]
 background = /usr/local/etc/lightdm/wallpaper/0062.jpg
@@ -101,7 +122,9 @@ clock-format = %I:%M %p
 theme-name = ClassicLooks Solaris
 icon-theme-name = Flatery-Black
 EOF
+
 # Update FreeBSD base.
 freebsd-update fetch install
+
 # Reboot
 shutdown -r now
