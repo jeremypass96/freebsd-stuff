8cfef5108b4a4f57e218c6349177fc42bee7432c
Jeremy Passarelli <jeremy.pass.101@aol.com>
2021-12-07 15:31:31 -0500

    .

--- setup_windowmaker.sh
+++ setup_windowmaker.sh
@@ -167,6 +167,10 @@ sed -i '' s/#focus_password^no/focus_password^yes/g /usr/local/etc/slim.conf
 sed -i '' s/#auto_login^no/auto_login^yes/g /usr/local/etc/slim.conf
 sed -i '' s/current_theme^default/current_theme^slim-freebsd-dark-theme/g /usr/local/etc/slim.conf
 
+# Replace LightDM in rc.conf with SLiM.
+service delete lightdm
+service add slim
+
 # Disable unneeded TTYs and secure the rest. This will make you enter root's password when booting into single user mode, but you can't login as root while booted into normal mode.
 sed -i '' s/ttyu0/#ttyu0/g /etc/ttys
 sed -i '' s/ttyu1/#ttyu1/g /etc/ttys
