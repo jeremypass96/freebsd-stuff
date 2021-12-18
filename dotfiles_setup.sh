7578980387f68ec7b6134cf0d2800a2d84fd89da
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2021-12-18 16:40:20 -0500

    Update dotfiles_setup.sh

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,29 +1,39 @@
 #!/bin/sh
 
+# Checking to see if we're running as root.
+if [ $(id -u) -ne 0 ] ; then
+echo "Please run this setup script as root via 'su'! Thanks."
+exit
+fi
+
 # Final setup stage. Run as non-root user!
 cd Dotfiles/
 
 # Copy over .profile & fix system profile file.
-cp -v .profile ~
-sudo cp -v .profile /usr/share/skel/dot.profile
-sudo echo "EDITOR=micro;    export EDITOR" >> /etc/profile
-sudo echo "PAGER=less;   export PAGER" >> /etc/profile
-sudo echo "MANPAGER=less;    export MANPAGER" >> /etc/profile
-sudo echo 'QT_QPA_PLATFORMTHEME="qt5ct";   export QT_QPA_PLATFORMTHEME' >> /etc/profile
+cp -v .profile /home/$USER
+chown $USER .profile
+cp -v .profile /usr/share/skel/dot.profile
+echo "EDITOR=micro;    export EDITOR" >> /etc/profile
+echo "PAGER=less;   export PAGER" >> /etc/profile
+echo "MANPAGER=less;    export MANPAGER" >> /etc/profile
+echo 'QT_QPA_PLATFORMTHEME="qt5ct";   export QT_QPA_PLATFORMTHEME' >> /etc/profile
 
 # Copy over zsh config.
-cp -v .zshrc ~
-sudo cp -v .zshrc /usr/share/skel/dot.zshrc
+cp -v .zshrc /home/$USER
+chown $USER .zshrc
+cp -v .zshrc /usr/share/skel/dot.zshrc
 
 # Copy over neofetch config.
 mkdir -p ~/.config/neofetch
 cp -v config/neofetch/config.conf ~/.config/neofetch/
+chown $USER ~/.config/neofetch/*
 mkdir -p /usr/share/skel/dot.config/neofetch
 cp -v config/neofetch/config.conf /usr/share/skel/dot.config/neofetch/
 
 # Copy over micro config.
 mkdir -p ~/.config/micro
 cp -v config/micro/settings.json ~/.config/micro/
+chown $USER ~/.config/micro/*
 mkdir -p /usr/share/skel/dot.config/micro
 cp -v config/micro/settings.json /usr/share/skel/dot.config/micro/
 
@@ -31,5 +41,5 @@ cp -v config/micro/settings.json /usr/share/skel/dot.config/micro/
 chsh -s /usr/local/bin/zsh $USER
 
 # Get "zsh-autosuggestions" and "zsh-syntax-highlighting" Oh My Zsh plugins.
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
