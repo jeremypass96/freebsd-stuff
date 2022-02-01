5de50da6edd3081b4f5d93d6d9ca6c3d37fbf2a4
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-01-31 22:18:43 -0500

    Create gnome_gsettings.sh

---
+++ gnome_gsettings.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+gsettings set org.gnome.desktop.interface clock-format 12h
+gsettings set org.gnome.desktop.background show-desktop-icons true
+gsettings set org.gnome.desktop.background picture-uri /usr/local/share/backgrounds/gnome/Loveles.jpg
+gsettings set org.gnome.desktop.interface font-name "Roboto 10"
+gsettings set org.gnome.desktop.interface gtk-theme "Pop"
+gsettings set org.gnome.desktop.interface toolbar-style icons
+gsettings set org.gnome.desktop.interface icon-theme "Newaita-reborn-pop-os"
+gsettings set org.gnome.desktop.interface cursor-theme "macOSBigSur"
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled false
+gsettings set org.gnome.desktop.sound input-feedback-sounds true
+gsettings set org.gnome.desktop.datetime automatic-timezone true
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true
