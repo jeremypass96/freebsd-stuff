7139fbf2873ecca1d5e8ae8c7f7be32bd9641502
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-03-19 12:22:48 -0400

    Update setup_mate.sh

--- setup_mate.sh
+++ setup_mate.sh
@@ -33,10 +33,10 @@ echo "proc           /proc        procfs    rw      0     0" >> /etc/fstab
 
 # Install packages.
 pkg install -y sudo xorg-minimal xorg-drivers xorg-fonts xorg-libraries noto-basic noto-emoji mate xfburn parole firefox thunderbird audacity handbrake isomaster abiword gnumeric transmission-gtk asunder gimp inkscape pinta shotwell webfonts virtualbox-ose micro xclip zsh ohmyzsh neofetch lightdm slick-greeter mp4v2 classiclooks wine wine-mono wine-gecko numlockx devcpu-data automount unix2dos smartmontools ubuntu-font office-code-pro webfonts droid-fonts-ttf materialdesign-ttf roboto-fonts-ttf xdg-user-dirs duf
-fi
 
 # Setup rc.conf file.
 ./rcconf_setup.sh
+fi
 
 if [ $answer = "ports" ] ; then
 
