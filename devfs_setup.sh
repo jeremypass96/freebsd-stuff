654f104c8d5ed13fdedffa7224d770b6d2a16a2e
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-03-21 14:15:52 -0400

    Update devfs_setup.sh

--- devfs_setup.sh
+++ devfs_setup.sh
@@ -2,31 +2,31 @@
 # Sets up a devfs.rules/.conf file on FreeBSD for FreeBSD desktop use.
 cat << EOF > /etc/devfs.rules
 [devfsrules_common=7]
-add path 'ad*' mode 666
-add path 'da*' mode 666
-add path 'acd*' mode 666
-add path 'cd*' mode 666
-add path 'mmcsd*' mode 666
-add path 'pass*' mode 666
-add path 'xpt*' mode 666
-add path 'ugen*' mode 666
-add path 'usbctl' mode 666
-add path 'usb*' mode 666
-add path 'lpt*' mode 666
-add path 'ulpt*' mode 666
-add path 'unlpt*' mode 666
-add path 'fd*' mode 666
-add path 'uscan*' mode 666
-add path 'video*' mode 666
+add path 'ad*' mode 0660
+add path 'da*' mode 0660
+add path 'acd*' mode 0660
+add path 'cd*' mode 0660
+add path 'mmcsd*' mode 0660
+add path 'pass*' mode 0660
+add path 'xpt*' mode 0660
+add path 'ugen*' mode 0660
+add path 'usbctl' mode 0660
+add path 'usb*' mode 0660
+add path 'lpt*' mode 0660
+add path 'ulpt*' mode 0660
+add path 'unlpt*' mode 0660
+add path 'fd*' mode 0660
+add path 'uscan*' mode 0660
+add path 'video*' mode 0660
 EOF
 sysrc devfs_system_ruleset="devfsrules_common"
 #
 sed -i '' s/"#link	cd0	cdrom/link	cd0	cdrom"/g /etc/devfs.conf
 sed -i '' s/"#link	cd0	dvd/link	cd0	dvd"/g /etc/devfs.conf
-echo "perm /dev/acd* 0666" >> /etc/devfs.conf
-echo "perm /dev/cd* 0666" >> /etc/devfs.conf
-echo "perm /dev/pass* 0666" >> /etc/devfs.conf
-echo "perm /dev/xpt* 0666" >> /etc/devfs.conf
-echo "perm /dev/da* 0666" >> /etc/devfs.conf
-echo "perm /dev/uscanner* 0666" >> /etc/devfs.conf
-echo "perm /dev/video* 0666" >> /etc/devfs.conf
+echo "perm /dev/acd* 0660" >> /etc/devfs.conf
+echo "perm /dev/cd* 0660" >> /etc/devfs.conf
+echo "perm /dev/pass* 0660" >> /etc/devfs.conf
+echo "perm /dev/xpt* 0660" >> /etc/devfs.conf
+echo "perm /dev/da* 0660" >> /etc/devfs.conf
+echo "perm /dev/uscanner* 0660" >> /etc/devfs.conf
+echo "perm /dev/video* 0660" >> /etc/devfs.conf
