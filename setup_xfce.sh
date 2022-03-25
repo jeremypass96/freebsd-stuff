2866a5f33defef3c7059848a38ded94589e075b2
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-03-25 17:38:44 -0400

    Update setup_xfce.sh

--- setup_xfce.sh
+++ setup_xfce.sh
@@ -60,11 +60,19 @@ portsnap auto
 
 read -p "Do you plan to use a printer? (y/n) " resp
 if [ 0"$resp" = 0y ]; then
+sed -i '' '13s/$/ CUPS/' /etc/make.conf
 cd /usr/ports/print/cups && make install clean
-cd /usr/ports/print/papersize-default-letter && make install clean
 sysrc cupsd_enable="YES"
+read -p "Paper size? (Letter/A4) " resp
+if [ 0"$resp" = 0Letter ]; then
+cd /usr/ports/print/papersize-default-letter && make install clean
+fi
+if [ 0"$resp" = 0A4 ]; then
+cd /usr/ports/print/papersize-default-a4 && make install clean
+fi
 fi
 if [ 0"$resp" = 0n ]; then
+sed -i '' '14s/$/ CUPS/' /etc/make.conf
 continue
 fi
 
