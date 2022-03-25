aaa93baac780a323eefa6472cb630aad5d0f4eb2
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-03-25 18:00:00 -0400

    Update setup_katana.sh

--- setup_katana.sh
+++ setup_katana.sh
@@ -9,7 +9,11 @@ fi
 
 clear
 
-echo "Welcome to the FreeBSD Katana setup script. This script will setup Xorg, the Katana desktop, some useful software for you, along with the rc.conf file being tweaked for desktop use."
+echo "Welcome to the FreeBSD Katana setup script."
+echo "This script will setup Xorg, the Katana desktop, some useful software for you, along with the rc.conf file being tweaked for desktop use."
+read -p "Press any key to continue..." resp
+
+clear
 
 # Update repo to use latest packages.
 mkdir -p /usr/local/etc/pkg/repos
@@ -17,6 +21,7 @@ echo 'FreeBSD: { url: "http://pkg0.nyi.FreeBSD.org/${ABI}/latest", mirror_type: 
 echo 'Katana: { url: "pkg+https://raw.githubusercontent.com/fluxer/katana-freebsd/master", mirror_type: "srv", enabled: yes }' > /usr/local/etc/pkg/repos/Katana.conf
 pkg update
 
+echo ""
 read -p "Do you plan to use a printer? (y/n) " resp
 if [ 0"$resp" = 0y ]; then
 pkg install -y cups
@@ -32,6 +37,8 @@ fi
 if [ 0"$resp" = 0n ]; then
 continue
 fi
+
+clear
 
 # Install packages.
 pkg install -y sudo xorg-minimal xorg-drivers xorg-fonts xorg-libraries noto-basic noto-emoji katana-workspace katana-extraapps Kvantum-qt5 firefox audacity handbrake isomaster abiword gnumeric transmission-gtk asunder gimp inkscape pinta shotwell webfonts virtualbox-ose micro xclip zsh ohmyzsh neofetch mp4v2 wine wine-mono wine-gecko numlockx devcpu-data automount unix2dos smartmontools ubuntu-font sourcecodepro-ttf webfonts droid-fonts-ttf materialdesign-ttf roboto-fonts-ttf xdg-user-dirs duf
