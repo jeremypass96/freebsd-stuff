5a2c286dcd2b3ff8bb2827265e5c1b2c54170cd5
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-04-01 14:06:43 -0400

    Update devfs_setup.sh

--- devfs_setup.sh
+++ devfs_setup.sh
@@ -2,31 +2,47 @@
 # Sets up a devfs.rules/.conf file on FreeBSD for FreeBSD desktop use.
 cat << EOF > /etc/devfs.rules
 [devfsrules_common=7]
-add path 'ad*' mode 0660
-add path 'da*' mode 0660
-add path 'acd*' mode 0660
-add path 'cd*' mode 0660
-add path 'mmcsd*' mode 0660
-add path 'pass*' mode 0660
-add path 'xpt*' mode 0660
-add path 'ugen*' mode 0660
-add path 'usbctl' mode 0660
-add path 'usb*' mode 0660
-add path 'lpt*' mode 0660
-add path 'ulpt*' mode 0660
-add path 'unlpt*' mode 0660
-add path 'fd*' mode 0660
-add path 'uscan*' mode 0660
-add path 'video*' mode 0660
+add path 'ad[0-9]\*'	mode 666
+add path 'ada[0-9]\*'	mode 666
+add path 'da[0-9]\*'	mode 666
+add path 'acd[0-9]\*'	mode 666
+add path 'cd[0-9]\*'	mode 666
+add path 'mmcsd[0-9]\*'	mode 666
+add path 'pass[0-9]\*'	mode 666
+add path 'xpt0'			mode 666
+add path 'ugen[0-9]\*'	mode 666
+add path 'usbctl'		mode 666
+add path 'usb/\*'		mode 666
+add path 'lpt[0-9]\*'	mode 666
+add path 'ulpt[0-9]\*'	mode 666
+add path 'unlpt[0-9]\*'	mode 666
+add path 'fd[0-9]\*'	mode 666
+add path 'uscan[0-9]\*'	mode 666
+add path 'video[0-9]\*'	mode 666
+add path 'tuner[0-9]*'  mode 666
+add path 'dvb/\*'		mode 666
+add path 'cx88*' 		mode 0660
+add path 'iicdev*' 		mode 0660
+add path 'uvisor[0-9]*' mode 0660
 EOF
 sysrc devfs_system_ruleset="devfsrules_common"
 #
 sed -i '' s/"#link cd0 cdrom/link	cd0	cdrom"/g /etc/devfs.conf
 sed -i '' s/"#link cd0 dvd/link	cd0	dvd"/g /etc/devfs.conf
-echo "perm /dev/acd* 0660" >> /etc/devfs.conf
-echo "perm /dev/cd* 0660" >> /etc/devfs.conf
-echo "perm /dev/pass* 0660" >> /etc/devfs.conf
-echo "perm /dev/xpt* 0660" >> /etc/devfs.conf
-echo "perm /dev/da* 0660" >> /etc/devfs.conf
-echo "perm /dev/uscanner* 0660" >> /etc/devfs.conf
-echo "perm /dev/video* 0660" >> /etc/devfs.conf
+#
+echo "# Allow all users to access CD/DVD drives" >> /etc/devfs.conf
+echo "perm 	/dev/acd* 					0666" >> /etc/devfs.conf
+echo "perm 	/dev/cd* 					0666" >> /etc/devfs.conf
+
+echo "# Allow all useres to access USB devices" >> /etc/devfs.conf
+echo "perm 	/dev/da* 					0666" >> /etc/devfs.conf
+
+echo "# Misc. other devices" >> /etc/devfs.conf
+echo "perm 	/dev/pass* 					0666" >> /etc/devfs.conf
+echo "perm 	/dev/xpt0 					0666" >> /etc/devfs.conf
+echo "perm 	/dev/uscanner* 				0666" >> /etc/devfs.conf
+echo "perm 	/dev/video* 				0666" >> /etc/devfs.conf
+echo "perm 	/dev/tuner0 				0666" >> /etc/devfs.conf
+echo "perm    /dev/dvb/adapter0/demux0    	0666" >> /etc/devfs.conf
+echo "perm    /dev/dvb/adapter0/dvr       	0666" >> /etc/devfs.conf
+echo "perm    /dev/dvb/adapter0/frontend0 	0666" >> /etc/devfs.conf
