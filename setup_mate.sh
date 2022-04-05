63c05700d15cc486dad146dd8e28ba89b20d5d06
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-04-05 18:10:55 -0400

    Update setup_mate.sh

--- setup_mate.sh
+++ setup_mate.sh
@@ -222,6 +222,6 @@ show-hostname = true
 show-a11y = false
 show-keyboard = false
 clock-format = %I:%M %p
-theme-name = Papirus
-icon-theme-name = Skeuos-Red-Light
+theme-name = Papirus-Light
+icon-theme-name = Skeuos-Blue-Light
 EOF
