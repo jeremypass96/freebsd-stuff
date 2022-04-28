8cb4ac7b4d0cab1e72665733271dee35a675777d
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-04-28 15:03:07 -0400

    Update desktop-install_pkg_removal.sh

--- desktop-install_pkg_removal.sh
+++ desktop-install_pkg_removal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo pkg remove -y xorg xdm xsm xf86-video-mga && sudo pkg autoremove -y
+sudo pkg remove -y xorg xdm xsm xf86-video-mga; sudo pkg autoremove -y
 sudo pkg install -y xorg-minimal xorg-fonts xorg-libraries xorg-drivers
