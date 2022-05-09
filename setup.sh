d4649513e11338617541242a81d518c9635242e0
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-09 11:06:49 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Checking to see if we're running as root.
-if [ $(id -u) -ne 0 ] ; then
+if [ $(id -u) -ne 0 ]; then
 echo "Please run this setup script as root via 'su'! Thanks."
 exit
 fi
