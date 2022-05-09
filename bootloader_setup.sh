2b3d3926d7cfc8edc134e22dd3bc3771879babfe
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-09 11:02:53 -0400

    Update bootloader_setup.sh

--- bootloader_setup.sh
+++ bootloader_setup.sh
@@ -2,7 +2,7 @@
 # This shell script sets up FreeBSD's loader.conf bootloader variables for desktop use.
 
 # Checking to see if we're running as root.
-if [ $(id -u) -ne 0 ] ; then
+if [ $(id -u) -ne 0 ]; then
 echo "Please run this bootloader setup script as root! Thanks."
 exit
 fi
