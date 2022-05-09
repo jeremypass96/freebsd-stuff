654aed47919bf6be94d8aacc876a292b22aac585
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-09 11:15:28 -0400

    Update sysctl_setup.sh

--- sysctl_setup.sh
+++ sysctl_setup.sh
@@ -2,7 +2,7 @@
 # This shell script sets up FreeBSD's sysctl.conf kernel variables for desktop use.
 
 # Checking to see if we're running as root.
-if [ $(id -u) -ne 0 ] ; then
+if [ $(id -u) -ne 0 ]; then
 echo "Please run this script as root! Thanks."
 exit
 fi
