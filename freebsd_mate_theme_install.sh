e7a5cc604c2be0a81a7b805635a9a4c79d37a417
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-01 17:25:33 -0400

    Update freebsd_mate_theme_install.sh

--- freebsd_mate_theme_install.sh
+++ freebsd_mate_theme_install.sh
@@ -5,7 +5,7 @@ clear
 # Get wallpaper!
 echo "Getting wallpaper..."
 fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0188.jpg?inline=false -o /usr/local/share/backgrounds/0188.jpg
-sudo chown root:wheel /usr/local/share/backgrounds/0188.jpg
+doas chown root:wheel /usr/local/share/backgrounds/0188.jpg
 clear
 # Set wallpaper.
 gsettings set org.mate.background picture-options zoom && gsettings set org.mate.background picture-filename /usr/local/share/backgrounds/0188.jpg
