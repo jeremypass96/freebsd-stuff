afce344ad3069ff947655642da3b4352a22c9462
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-01 17:24:47 -0400

    Update freebsd_symlinks.sh

--- freebsd_symlinks.sh
+++ freebsd_symlinks.sh
@@ -2,9 +2,9 @@
 
 # Symlink bash executable to /bin/bash so that scripts written on/for Linux can run properly.
 echo "Making a bash symlink to /bin/bash for Linux shell script compatability..."
-sudo ln -sv /usr/local/bin/bash /bin/bash
+doas ln -sv /usr/local/bin/bash /bin/bash
 
 # Symlink themes/icons directories to enable installation of Linux themes.
 echo "Symlinking "/usr/local/share/themes" and "/usr/local/share/icons" to /usr/share/ for easy Linux themes and icons installation..."
-sudo ln -sv /usr/local/share/themes /usr/share
-sudo ln -sv /usr/local/share/icons /usr/share
+doas ln -sv /usr/local/share/themes /usr/share
+doas ln -sv /usr/local/share/icons /usr/share
