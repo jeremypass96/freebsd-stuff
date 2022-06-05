21dea6060f917483e5685bd724d8f023a7f68d1d
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-05 12:17:30 -0400

    Update software_dialog_pkgs.sh

--- software_dialog_pkgs.sh
+++ software_dialog_pkgs.sh
@@ -1,19 +1,18 @@
 #!/usr/local/bin/bash
-cmd=(dialog --separate-output --no-cancel --checklist "Would you like to install any extra 3rd party software?" 22 70 22)
+cmd=(dialog --separate-output --no-cancel --checklist "Would you like to install any extra 3rd party software?" 21 70 21)
 options=(1 "Audacity (audio editor)" off
-         2 "Xfburn (CD burner)" off
-         3 "Handbrake (video file converter)" off
-         4 "ISO Master (ISO file editor)" off
-         5 "AbiWord (word processor)" off
-         6 "Gnumeric (speadsheet)" off
-         7 "Transmission (torrent downloader)" off
-         8 "Asunder (CD ripper)" off
-         9 "GIMP (image editor)" off
-         10 "Inkskape (vector graphics editor)" off
-         11 "Pinta (image editor similar to Paint.NET on Windows)" off
-         12 "Shotwell (photo organizer/editor)" off
-         13 "VirtualBox (run multiple operating systems on your PC)" off
-         14 "Wine (run Windows applications)" off
+         2 "Handbrake (video file converter)" off
+         3 "ISO Master (ISO file editor)" off
+         4 "AbiWord (word processor)" off
+         5 "Gnumeric (speadsheet)" off
+         6 "Transmission (torrent downloader)" off
+         7 "Asunder (CD ripper)" off
+         8 "GIMP (image editor)" off
+         9 "Inkskape (vector graphics editor)" off
+         10 "Pinta (image editor similar to Paint.NET on Windows)" off
+         11 "Shotwell (photo organizer/editor)" off
+         12 "VirtualBox (run multiple operating systems on your PC)" off
+         13 "Wine (run Windows applications)" off
          n "No, don't install any 3rd party software." off)
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
@@ -21,25 +20,24 @@ for choice in $choices
 do
     case $choice in
         1) pkg="audacity";;
-        2) pkg="xfburn";;
-        3) pkg="handbrake";;
-        4) pkg="isomaster";;
-        5) pkg="abiword";;
-        6) pkg="gnumeric";;
-        7) pkg="transmission-gtk";;
-        8) pkg="asunder";;
-        9) pkg="gimp";;
-        10) pkg="inkscape";;
-        11) pkg="pinta";;
-        12) pkg="shotwell";;
-        13) pkg="virtualbox-ose"
+        2) pkg="handbrake";;
+        3) pkg="isomaster";;
+        4) pkg="abiword";;
+        5) pkg="gnumeric";;
+        6) pkg="transmission-gtk";;
+        7) pkg="asunder";;
+        8) pkg="gimp";;
+        9) pkg="inkscape";;
+        10) pkg="pinta";;
+        11) pkg="shotwell";;
+        12) pkg="virtualbox-ose"
         sysrc vboxnet_enable="YES"
         echo "### VirtualBox stuff ###" >> /etc/sysctl.conf
         echo vfs.aio.max_buf_aio=8192 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_queue_per_proc=65536 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_per_proc=8192 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_queue=65536 >> /etc/sysctl.conf;;
-        14) pkg="wine wine-mono wine-gecko"
+        13) pkg="wine wine-mono wine-gecko"
         echo "# Wine fix" >> /boot/loader.conf
         echo machdep.max_ldt_segment=2048 >> /boot/loader.conf;;
         n) continue
