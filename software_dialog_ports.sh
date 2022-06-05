dfb986b52179e9bfdfbf2d7f8bae61a55d486ef5
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-05 12:12:59 -0400

    Update software_dialog_ports.sh

--- software_dialog_ports.sh
+++ software_dialog_ports.sh
@@ -1,19 +1,18 @@
 #!/usr/local/bin/bash
 cmd=(dialog --separate-output --no-cancel --checklist "Would you like to install any extra 3rd party software?" 22 70 22)
 options=(1 "Audacity (audio editor)" off
-         2 "Xfburn (CD burner)" off
-         3 "Handbrake (video file converter)" off
-         4 "ISO Master (ISO file editor)" off
-         5 "AbiWord (word processor)" off
-         6 "Gnumeric (speadsheet)" off
-         7 "Transmission (torrent downloader)" off
-         8 "Asunder (CD ripper)" off
-         9 "GIMP (image editor)" off
-         10 "Inkskape (vector graphics editor)" off
-         11 "Pinta (image editor similar to Paint.NET on Windows)" off
-         12 "Shotwell (photo organizer/editor)" off
-         13 "VirtualBox (run multiple operating systems on your PC)" off
-         14 "Wine (run Windows applications)" off
+         2 "Handbrake (video file converter)" off
+         3 "ISO Master (ISO file editor)" off
+         4 "AbiWord (word processor)" off
+         5 "Gnumeric (speadsheet)" off
+         6 "Transmission (torrent downloader)" off
+         7 "Asunder (CD ripper)" off
+         8 "GIMP (image editor)" off
+         9 "Inkskape (vector graphics editor)" off
+         10 "Pinta (image editor similar to Paint.NET on Windows)" off
+         11 "Shotwell (photo organizer/editor)" off
+         12 "VirtualBox (run multiple operating systems on your PC)" off
+         13 "Wine (run Windows applications)" off
          n "No, don't install any 3rd party software." off)
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
@@ -21,25 +20,24 @@ for choice in $choices
 do
     case $choice in
         1) port="audio/audacity";;
-        2) port="sysutils/xfburn";;
-        3) port="multimedia/handbrake";;
-        4) port="sysutils/somaster";;
-        5) port="editors/abiword";;
-        6) port="math/gnumeric";;
-        7) port="net-p2p/transmission-gtk";;
-        8) port="audio/asunder";;
-        9) port="graphics/gimp";;
-        10) port="graphics/inkscape";;
-        11) port="graphics/pinta";;
-        12) port="graphics/shotwell";;
-        13) port="emulators/virtualbox-ose"
+        2) port="multimedia/handbrake";;
+        3) port="sysutils/somaster";;
+        4) port="editors/abiword";;
+        5) port="math/gnumeric";;
+        6) port="net-p2p/transmission-gtk";;
+        7) port="audio/asunder";;
+        8) port="graphics/gimp";;
+        9) port="graphics/inkscape";;
+        10) port="graphics/pinta";;
+        11) port="graphics/shotwell";;
+        12) port="emulators/virtualbox-ose"
         sysrc vboxnet_enable="YES"
         echo "### VirtualBox stuff ###" >> /etc/sysctl.conf
         echo vfs.aio.max_buf_aio=8192 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_queue_per_proc=65536 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_per_proc=8192 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_queue=65536 >> /etc/sysctl.conf;;
-        14) port="emulators/wine"
+        13) port="emulators/wine"
         echo "# Wine fix" >> /boot/loader.conf
         echo machdep.max_ldt_segment=2048 >> /boot/loader.conf;;
         n) continue
