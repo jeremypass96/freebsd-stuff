63d3f05ecd18378503ffb3b410e8f43ce74b7fab
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-22 19:12:30 -0400

    Update freebsd_symlinks.sh

--- freebsd_symlinks.sh
+++ freebsd_symlinks.sh
@@ -12,5 +12,4 @@ ln -sv /usr/local/bin/bash /bin/bash
 
 # Symlink themes/icons directories to enable installation of Linux themes.
 echo "Symlinking "/usr/local/share/themes" and "/usr/local/share/icons" to /usr/share/ for easy Linux themes and icons installation..."
-ln -sv /usr/local/share/themes /usr/share
-ln -sv /usr/local/share/icons /usr/share
+ln -sv /usr/local/share/themes /usr/share ; ln -sv /usr/local/share/icons /usr/share
