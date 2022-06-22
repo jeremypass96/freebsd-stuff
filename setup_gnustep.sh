5a9515c4c0dcc4e29c8ad44ebfb6d5ebfbc25146
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-22 19:28:21 -0400

    Add files via upload

---
+++ setup_gnustep.sh
@@ -0,0 +1,232 @@
+#!/bin/sh
+# This script will set up a complete FreeBSD desktop for you, ready to go when you reboot.
+
+# Checking to see if we're running as root.
+if [ $(id -u) -ne 0 ]; then
+echo "Please run this setup script as root via 'su'! Thanks."
+exit
+fi
+
+clear
+
+echo "Welcome to the FreeBSD WindowMaker + GNUstep setup script."
+echo "This script will setup Xorg, WindowMaker + GNUstep, some useful software for you, along with the rc.conf file being tweaked for desktop use."
+echo ""
+read -p "Press the Enter key to continue..." resp
+
+clear
+
+read -p "Do you plan to install software via pkg (binary packages) or ports (FreeBSD Ports tree)? (pkg/ports): " resp
+if [ "$resp" = pkg ]; then
+
+# Update repo to use latest packages.
+mkdir -p /usr/local/etc/pkg/repos
+cat << EOF > /usr/local/etc/pkg/repos/FreeBSD.conf
+FreeBSD: {
+  url: "pkg+http://pkg.FreeBSD.org/\${ABI}/latest",
+  mirror_type: "srv",
+  signature_type: "fingerprints",
+  fingerprints: "/usr/share/keys/pkg",
+  enabled: yes
+}
+EOF
+pkg update
+
+clear
+
+read -p "Do you plan to use a printer? (y/n): " resp
+if [ "$resp" = y ]; then
+pkg install -y cups gutenprint system-config-printer hplip
+sysrc cupsd_enable="YES"
+sysrc cups_browsed_enable="YES"
+sed -i '' s/JobPrivateAccess/#JobPrivateAccess/g /usr/local/etc/cups/cupsd.conf
+sed -i '' s/JobPrivateValues/#JobPrivateValues/g /usr/local/etc/cups/cupsd.conf
+read -p "Paper size? (Letter/A4): " resp
+if [ "$resp" = Letter ]; then
+pkg install -y papersize-default-letter
+fi
+if [ "$resp" = A4 ]; then
+pkg install -y papersize-default-a4
+fi
+fi
+if [ "$resp" = n ]; then
+continue
+fi
+
+clear
+
+# Install packages.
+pkg install -y bash doas xorg-minimal xorg-drivers xorg-fonts xorg-libraries windowmaker wdm wmforecast gnustep-app noto-basic noto-emoji skeuos-gtk-themes papirus-icon-theme font-manager qt5ct qt5-style-plugins chromium webfonts micro xclip zsh ohmyzsh neofetch octopkg lightdm slick-greeter mp4v2 numlockx devcpu-data automount fusefs-simple-mtpfs unix2dos smartmontools ubuntu-font webfonts droid-fonts-ttf materialdesign-ttf roboto-fonts-ttf plex-ttf xdg-user-dirs duf colorize freedesktop-sound-theme
+
+clear
+
+# Setup rc.conf file.
+./rcconf_setup.sh
+
+# Install 3rd party software.
+./software_dialog_pkgs.sh
+pkg clean -y
+
+# Install BSDstats.
+read -p "Would you like to enable BSDstats? (y/n): " resp
+if [ "$resp" = y ]; then
+pkg install -y bsdstats
+sysrc bsdstats_enable="YES"
+if [ "$resp" = n ]; then
+continue
+fi
+
+if [ "$resp" = ports ]; then
+
+# Copying over make.conf file.
+cp -v make.conf /etc/
+
+# Configure the MAKE_JOBS_NUMBER line in make.conf
+sed -i '' s/MAKE_JOBS_NUMBER=/MAKE_JOBS_NUMBER=`sysctl -n hw.ncpu`/g /etc/make.conf
+
+# Avoid pulling in Ports tree categories with non-English languages.
+sed -i '' s/'#REFUSE arabic chinese french german hebrew hungarian japanese'/'REFUSE arabic chinese french german hebrew hungarian japanese'/g /etc/portsnap.conf
+sed -i '' s/'#REFUSE korean polish portuguese russian ukrainian vietnamese'/'REFUSE korean polish portuguese russian ukrainian vietnamese'/g /etc/portsnap.conf
+
+# Pull in Ports tree, extract, and update it.
+portsnap auto
+
+clear
+
+read -p "Do you plan to use a printer? (y/n): " resp
+if [ "$resp" = y ]; then
+sed -i '' '13s/$/ CUPS/' /etc/make.conf
+sed -i '' '23s/$/print_hplip_UNSET=X11/' /etc/make.conf
+echo "" >> /etc/make.conf
+cd /usr/ports/print/cups && make install clean
+cd /usr/ports/print/gutenprint && make install clean
+cd /usr/ports/print/system-config-printer && make install clean
+cd /usr/ports/print/hplip && make install clean
+sysrc cupsd_enable="YES"
+sysrc cups_browsed_enable="YES"
+sed -i '' s/JobPrivateAccess/#JobPrivateAccess/g /usr/local/etc/cups/cupsd.conf
+sed -i '' s/JobPrivateValues/#JobPrivateValues/g /usr/local/etc/cups/cupsd.conf
+read -p "Paper size? (Letter/A4): " resp
+if [ "$resp" = Letter ]; then
+cd /usr/ports/print/papersize-default-letter && make install clean
+fi
+if [ "$resp" = A4 ]; then
+cd /usr/ports/print/papersize-default-a4 && make install clean
+fi
+fi
+if [ "$resp" = n ]; then
+sed -i '' '14s/$/ CUPS/' /etc/make.conf
+continue
+fi
+
+clear
+
+# Install Ports.
+cd /usr/ports/shells/bash && make install clean
+cd /usr/ports/security/doas && make install clean
+cd /usr/ports/editors/micro && make install clean
+cd /usr/ports/x11/xclip && make install clean
+cd /usr/ports/shells/zsh && make install clean
+cd /usr/ports/shells/ohmyzsh && make install clean
+cd /usr/ports/sysutils/neofetch && make install clean
+cd /usr/ports/x11/xorg && make install clean
+cd /usr/ports/x11-wm/windowmaker && make install clean
+cd /usr/ports/x11/wdm && make install clean
+cd /usr/ports/misc/wmforecast && make install clean
+cd /usr/ports/x11/gnustep-app && make install clean
+cd /usr/ports/x11-themes/skeuos-gtk-themes && make install clean
+cd /usr/ports/x11-themes/papirus-icon-theme && make install clean
+cd /usr/ports/x11-fonts/font-manager && make install clean
+cd /usr/ports/misc/qt5ct && make install clean
+cd /usr/ports/x11-themes/qt5-style-plugins && make install clean
+cd /usr/ports/www/chromium && make install clean
+cd /usr/ports/x11-fonts/noto && make install clean
+cd /usr/ports/x11-fonts/webfonts && make install clean
+cd /usr/ports/sysutils/gksu && make install clean
+cd /usr/ports/x11/slick-greeter && make install clean
+cd /usr/ports/multimedia/mp4v2 && make install clean
+cd /usr/ports/x11/numlockx && make install clean
+cd /usr/ports/sysutils/devcpu-data && make install clean
+cd /usr/ports/sysutils/automount && make install clean
+cd /usr/ports/sysutils/fusefs-simple-mtpfs && make install clean
+cd /usr/ports/converters/unix2dos && make install clean
+cd /usr/ports/sysutils/smartmontools && make install clean
+cd /usr/ports/x11-fonts/ubuntu-font && make install clean
+cd /usr/ports/x11-fonts/webfonts && make install clean
+cd /usr/ports/x11-fonts/droid-fonts-ttf && make install clean
+cd /usr/ports/x11-fonts/materialdesign-ttf && make install clean
+cd /usr/ports/x11-fonts/roboto-fonts-ttf && make install clean
+cd /usr/ports/x11-fonts/plex-ttf && make install clean
+cd /usr/ports/devel/xdg-user-dirs && make install clean
+cd /usr/ports/sysutils/duf && make install clean
+cd /usr/ports/sysutils/colorize && make install clean
+cd /usr/ports/ports-mgmt/portmaster && make install clean
+
+# Setup rc.conf file.
+cd /home/$USER/freebsd-setup-scripts
+./rcconf_setup_ports.sh
+
+# Install 3rd party software.
+./software_dialog_ports.sh
+
+# Install BSDstats.
+read -p "Would you like to enable BSDstats? (y/n): " resp
+if [ "$resp" = y ]; then
+portmaster --no-confirm sysutils/bsdstats
+sysrc bsdstats_enable="YES"
+if [ "$resp" = n ]; then
+continue
+fi
+
+clear
+
+# Install cursor theme.
+echo "Installing the "Volantes Light Cursors" cursor theme..."
+tar -xvf volantes_light_cursors.tar.gz -C /usr/local/share/icons
+
+# Setup LightDM.
+sysrc lightdm_enable="YES"
+sed -i '' s/#pam-autologin-service=lightdm-autologin/pam-autologin-service=lightdm-autologin/g /usr/local/etc/lightdm/lightdm.conf
+sed -i '' s/#allow-user-switching=true/allow-user-switching=true/g /usr/local/etc/lightdm/lightdm.conf
+sed -i '' s/#allow-guest=true/allow-guest=false/g /usr/local/etc/lightdm/lightdm.conf
+sed -i '' s/"#greeter-setup-script=/greeter-setup-script=numlockx on"/g /usr/local/etc/lightdm/lightdm.conf
+sed -i '' s/#autologin-user=/autologin-user=$USER/g /usr/local/etc/lightdm/lightdm.conf
+sed -i '' s/#autologin-user-timeout=0/autologin-user-timeout=0/g /usr/local/etc/lightdm/lightdm.conf
+mkdir /usr/local/etc/lightdm/wallpaper
+fetch https://raw.githubusercontent.com/broozar/installDesktopFreeBSD/DarkMate13.0/files/wallpaper/centerFlat_grey-4k.png -o /usr/local/etc/lightdm/wallpaper/centerFlat_grey-4k.png
+chown root:wheel /usr/local/etc/lightdm/wallpaper/centerFlat_grey-4k.png
+
+# Setup slick greeter.
+cat << EOF > /usr/local/etc/lightdm/slick-greeter.conf
+[Greeter]
+background = /usr/local/etc/lightdm/wallpaper/centerFlat_grey-4k.png
+draw-user-backgrounds = false
+draw-grid = false
+show-hostname = true
+show-a11y = false
+show-keyboard = false
+clock-format = %I:%M %p
+theme-name = Skeuos-Blue-Light
+icon-theme-name = Papirus-Light
+EOF
+
+# Setup qt5ct and fix GTK/QT antialiasing.
+cp -v /home/$USER/freebsd-setup-scripts/Dotfiles/.xinitrc /home/$USER/.xinitrc
+cp -v /home/$USER/.xinitrc /usr/share/skel/.xinitrc
+chown $USER:$USER /home/$USER/.xinitrc
+
+# Setup qt5ct
+mkdir /home/$USER/.config/qt5ct
+chown $USER:$USER /home/$USER/.config/qt5ct/.
+mkdir /usr/share/skel/dot.config/qt5ct
+cp -v /home/$USER/freebsd-setup-scripts/Dotfiles/config/qt5ct/qt5ct.conf /home/$USER/.config/qt5ct/qt5ct.conf
+cp -v /home/$USER/.config/qt5ct/qt5ct.conf /usr/share/skel/dot.config/qt5ct/qt5ct.conf
+chown $USER:$USER /home/$USER/.config/qt5ct/qt5ct.conf
+
+# Hide "QT5 linguist" menu item.
+echo "Hidden=true" >> /usr/local/share/applications/usr_local_lib_qt5_bin_linguist.desktop
+
+# Fix user's .xinitrc permissions.
+chown $USER:$USER /home/$USER/.xinitrc
+# Fix user config directory permissions.
+chown $USER:$USER /home/$USER/.config
