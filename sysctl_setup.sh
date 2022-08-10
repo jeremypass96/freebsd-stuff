3141ca1ee88256a4311a70e03c8e56eec02ca475
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-08-10 10:39:26 -0400

    Update sysctl_setup.sh

--- sysctl_setup.sh
+++ sysctl_setup.sh
@@ -8,11 +8,19 @@ exit
 fi
 
 echo "#########################" >> /etc/sysctl.conf
+echo "# Improve responsiveness on high work load."
 echo kern.sched.preempt_thresh=224 >> /etc/sysctl.conf
+echo "# Disable terminal beep."
 echo kern.vt.enable_bell=0 >> /etc/sysctl.conf
+echo "# Enable users to mount drives."
 echo vfs.usermount=1 >> /etc/sysctl.conf
+echo "# Improve read/write permormance."
 echo vfs.read_max=128 >> /etc/sysctl.conf
+echo vfs.lorunningspace=1048576 >> /etc/sysctl.conf
+echo vfs.hirunningspace=5242880 >> /etc/sysctl.conf
+echo "# Disable creating *.core files in home directory."
 echo kern.coredump=0 >> /etc/sysctl.conf
+echo "# Speed up shutdown."
 echo kern.shutdown.poweroff_delay=2000 >> /etc/sysctl.conf
 echo kern.shutdown.kproc_shutdown_wait=20 >> /etc/sysctl.conf
 echo net.inet.ip.maxfragpackets=0 >> /etc/sysctl.conf
@@ -25,6 +33,10 @@ echo net.inet.ip.random_id=1 >> /etc/sysctl.conf
 echo net.inet.tcp.always_keepalive=0 >> /etc/sysctl.conf
 echo net.inet.tcp.fast_finwait2_recycle=1 >> /etc/sysctl.conf
 echo net.inet.ip.redirect=0 >> /etc/sysctl.conf
+echo net.inet.icmp.drop_redirect=1 >> /etc/sysctl.conf
+echo net.inet.ip.accept_sourceroute=0 >> /etc/sysctl.conf
+echo net.inet.ip.forwarding=0 >> /etc/sysctl.conf
+echo net.inet.ip.sourceroute=0 >> /etc/sysctl.conf
 echo net.inet.tcp.syncache.rexmtlimit=0 >> /etc/sysctl.conf
 echo net.inet.tcp.syncookies=0 >> /etc/sysctl.conf
 echo net.inet.tcp.cc.algorithm=cubic >> /etc/sysctl.conf
@@ -34,12 +46,14 @@ echo net.inet.ip.check_interface=1 >> /etc/sysctl.conf
 echo net.inet.ip.process_options=0 >> /etc/sysctl.conf
 echo net.inet.tcp.drop_synfin=1 >> /etc/sysctl.conf
 echo net.inet.tcp.icmp_may_rst=0 >> /etc/sysctl.conf
+echo net.inet.icmp.bmcastecho=0 >> /etc/sysctl.conf
 echo net.inet.tcp.nolocaltimewait=1 >> /etc/sysctl.conf
 echo net.inet.tcp.path_mtu_discovery=0 >> /etc/sysctl.conf
 echo net.inet6.icmp6.rediraccept=0 >> /etc/sysctl.conf
 echo net.inet6.ip6.redirect=0 >> /etc/sysctl.conf
 echo security.bsd.hardlink_check_gid=1 >> /etc/sysctl.conf
 echo security.bsd.hardlink_check_uid=1 >> /etc/sysctl.conf
+echo security.bsd.stack_guard_page=1 >> /etc/sysctl.conf
 echo kern.metadelay=2 >> /etc/sysctl.conf
 echo kern.dirdelay=3 >> /etc/sysctl.conf
 echo kern.filedelay=5 >> /etc/sysctl.conf
@@ -57,3 +71,7 @@ echo hw.spec_store_bypass_disable=2 >> /etc/sysctl.conf
 echo hw.mds_disable=3 >> /etc/sysctl.conf
 echo hw.ibrs_disable=0 >> /etc/sysctl.conf
 echo kern.sched.slice=3 >> /etc/sysctl.conf
+echo "# Fix stuttering issue on AMD CPUs."
+echo kern.sched.steal_thresh=1 >> /etc/sysctl.conf
+echo kern.maxfiles=100000 >> /etc/sysctl.conf
+echo kern.geom.part.mbr.enforce_chs=0 >> /etc/sysctl.conf
