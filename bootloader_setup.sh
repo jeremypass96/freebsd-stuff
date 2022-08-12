bb54446ffdda1a9bd42e788a231ae5dbb987daae
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-08-12 16:49:19 -0400

    Update bootloader_setup.sh

--- bootloader_setup.sh
+++ bootloader_setup.sh
@@ -10,15 +10,18 @@ fi
 sysrc -f /boot/loader.conf cuse_load="YES"
 sysrc -f /boot/loader.conf cc_cubic_load="YES"
 echo 'kern.random.fortuna.minpoolsize="512"' >> /boot/loader.conf
+echo "" >> /boot/loader.conf
 
 read -p "Do you have an AMD CPU installed in your computer? (y/n): " resp
 if [ "$resp" = y ]; then
 echo "# Load AMD southbridge watchdog timers and CPU thermal sensor." >> /boot/loader.conf
 sysrc -f /boot/loader.conf amdsbwd_load="YES"
 sysrc -f /boot/loader.conf amdtemp_load="YES"
+echo "" >> /boot/loader.conf
 fi
 if [ "$resp" = n ]; then
 sysrc -f /boot/loader.conf coretemp_load="YES"
+echo "" >> /boot/loader.conf
 fi
 
 # Disable boot menu/make booting faster.
@@ -26,24 +29,29 @@ echo "# Disable boot menu/make booting faster." >> /boot/loader.conf
 sysrc -f /boot/loader.conf loader_delay=0
 sysrc -f /boot/loader.conf autoboot_delay=-1
 sysrc -f /boot/loader.conf beastie_disable="YES"
+echo "" >> /boot/loader.conf
 
 # Driver for the AES and SHA accelerator on	x86 CPUs.
 echo "# Driver for the AES and SHA accelerator on	x86 CPUs." >> /boot/loader.conf
 sysrc -f /boot/loader.conf aesni_load="YES"
+echo "" >> /boot/loader.conf
 
 # Load GELI disk encryption.
 echo "# Load GELI disk encryption." >> /boot/loader.conf
 sysrc -f /boot/loader.conf crypto_load="YES"
 sysrc -f /boot/loader.conf geom_eli_load="YES"
+echo "" >> /boot/loader.conf
 
 # Hide boot messages.
 echo "# Hide boot messages." >> /boot/loader.conf
 sysrc -f /boot/loader.conf boot_mute="YES"
+echo "" >> /boot/loader.conf
 
 # Load CPU microcode.
 echo "# Load CPU microcode." >> /boot/loader.conf
 sysrc -f /boot/loader.conf cpu_microcode_load="YES"
 sysrc -f /boot/loader.conf cpu_microcode_name="/boot/firmware/intel-ucode.bin"
+echo "" >> /boot/loader.conf
 
 # Misc. other stuff.
 echo "# Misc. other stuff." >> /boot/loader.conf
@@ -56,23 +64,28 @@ echo kern.ipc.shmseg=10000 >> /boot/loader.conf
 echo kern.ipc.shmmni=10000 >> /boot/loader.conf
 echo kern.geom.label.disk_ident.enable=0 >> /boot/loader.conf
 echo kern.geom.label.gptid.enable=0 >> /boot/loader.conf
+echo "" >> /boot/loader.conf
 
 # Speed up bootup/shutdown.
 echo "# Speed up bootup/shutdown." >> /boot/loader.conf
 echo hw.usb.no_boot_wait=1 >> /boot/loader.conf
 echo hw.usb.no_shutdown_wait=1 >> /boot/loader.conf
+echo "" >> /boot/loader.conf
 
 # Protects against "Meltdown" security mitigation.
 echo '# Protects against "Meltdown" security mitigation.' >> /boot/loader.conf
 echo vm.pmap.pti=1 >> /boot/loader.conf
+echo "" >> /boot/loader.conf
 
 # Protects against "Varient 4" security mitigation.
 echo '# Protects against "Varient 4" security mitigation.' >> /boot/loader.conf
 echo hw.spec_store_bypass_disable_active=1 >> /boot/loader.conf
+echo "" >> /boot/loader.conf
 
 # Power off devices without an attached driver.
 echo "# Power off devices without an attached driver." >> /boot/loader.conf
 echo hw.pci.do_power_nodriver=2 >> /boot/loader.conf
+echo "" >> /boot/loader.conf
 
 echo "####################################################################################" >> /boot/loader.conf
 echo "##### Custom VT Console Colors - Andromeda (from iTerm2 Color Schemes project) #####" >> /boot/loader.conf
