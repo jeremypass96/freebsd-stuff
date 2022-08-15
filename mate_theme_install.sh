18ec2361fc346e56ad58e61b0db18588c82267be
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-08-15 19:43:12 -0400

    Update mate_theme_install.sh

--- mate_theme_install.sh
+++ mate_theme_install.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
+# Checking to see if we're running as a normal user.
+if [ $(whoami) != $USER ]; then
+echo "Please run this MATE post-install setup script as a normal user! Thanks."
+exit
+fi
+
 clear
-# Get wallpaper!
-echo "Getting wallpaper..."
-fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0188.jpg?inline=false -o /usr/local/share/backgrounds/0188.jpg
-sudo chown root:wheel /usr/local/share/backgrounds/0188.jpg
+
+# Get wallpapers!
+echo "Getting wallpapers..."
+cd /usr/local/share/backgrounds
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0004.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0011.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0023.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0036.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0037.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0042.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0057.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0058.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0065.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0076.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0188.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0230.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0252.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0256.jpg
+sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0257.jpg
+cd
 
 clear
 
@@ -12,11 +34,11 @@ gsettings set org.mate.background picture-options zoom && gsettings set org.mate
 # Set window titlebar font.
 gsettings set org.mate.Marco.general titlebar-font "Poppins Bold 10"
 # Set window theme.
-gsettings set org.mate.Marco.general theme "Skeuos-Blue-Light"
+gsettings set org.mate.Marco.general theme "Skeuos-Blue-Dark"
 # Turn off middle click on window titlebar.
 gsettings set org.mate.Marco.general action-middle-click-titlebar none
 # Set theme.
-gsettings set org.mate.interface gtk-theme "Skeuos-Blue-Light"
+gsettings set org.mate.interface gtk-theme "Skeuos-Blue-Dark"
 # Set icon theme.
 gsettings set org.mate.interface icon-theme Papirus-Light
 # Set fonts.
