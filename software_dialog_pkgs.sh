16dc8e51ec3aeda5c1358fdb0a80e4f5fb672bc6
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-08-17 19:14:52 -0400

    Update software_dialog_pkgs.sh

--- software_dialog_pkgs.sh
+++ software_dialog_pkgs.sh
@@ -33,7 +33,8 @@ do
         12) pkg="virtualbox-ose virtualbox-ose-kmod"
         sysrc vboxnet_enable="YES"
         sysrc kldload_vbox="vboxdrv"
-        echo "### VirtualBox stuff ###" >> /etc/sysctl.conf
+        echo "" >> /etc/sysctl.conf
+        echo "# VirtualBox stuff." >> /etc/sysctl.conf
         echo vfs.aio.max_buf_aio=8192 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_queue_per_proc=65536 >> /etc/sysctl.conf
         echo vfs.aio.max_aio_per_proc=8192 >> /etc/sysctl.conf
