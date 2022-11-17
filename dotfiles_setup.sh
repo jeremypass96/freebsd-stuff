60abd2d0662865dc520526de4daa132e005dc5e1
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-11-17 10:59:45 -0500

    Update dotfiles_setup.sh

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -45,3 +45,11 @@ chsh -s /usr/local/bin/zsh $USER
 # Get "zsh-autosuggestions" and "zsh-syntax-highlighting" Oh My Zsh plugins.
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/local/share/ohmyzsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/local/share/ohmyzsh/custom/plugins/zsh-syntax-highlighting
+
+# Copy over lsd config.
+mkdir -p /home/$USER/.config/lsd
+cp -v config/lsd/config.yaml /home/$USER/.config/lsd/
+cp -v config/lsd/config.yaml /root/.config/lsd/
+mkdir -p /usr/share/skel/dot.config/lsd
+cp -v config/lsd/config.yaml /usr/share/skel/dot.config/lsd/
+chown -R $USER:$USER /home/$USER/.config/lsd
