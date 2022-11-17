53f22ffee2c175f9c88ec475b75783b12789d003
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-11-17 10:39:09 -0500

    Update setup_mate.sh

--- setup_mate.sh
+++ setup_mate.sh
@@ -68,7 +68,7 @@ continue
 fi
 
 # Install packages.
-pkg install -y bash sudo xorg-minimal xorg-drivers xorg-fonts xorg-libraries noto-basic noto-emoji mate parole xfburn qt5ct qt5-style-plugins ulauncher chromium webfonts micro xclip zsh ohmyzsh neofetch octopkg lightdm slick-greeter mp4v2 skeuos-gtk-themes papirus-icon-theme numlockx devcpu-data automount fusefs-simple-mtpfs unix2dos smartmontools ubuntu-font webfonts droid-fonts-ttf materialdesign-ttf roboto-fonts-ttf plex-ttf xdg-user-dirs duf btop colorize freedesktop-sound-theme rkhunter chkrootkit topgrade
+pkg install -y bash sudo xorg-minimal xorg-drivers xorg-fonts xorg-libraries noto-basic noto-emoji mate parole xfburn qt5ct qt5-style-plugins ulauncher chromium webfonts micro xclip zsh ohmyzsh neofetch octopkg lightdm slick-greeter mp4v2 skeuos-gtk-themes papirus-icon-theme numlockx devcpu-data automount fusefs-simple-mtpfs unix2dos smartmontools ubuntu-font webfonts droid-fonts-ttf materialdesign-ttf roboto-fonts-ttf plex-ttf xdg-user-dirs duf btop colorize freedesktop-sound-theme rkhunter chkrootkit topgrade bat
 
 clear
 
@@ -188,6 +188,7 @@ cd /usr/ports/audio/freedesktop-sound-theme && sudo make install clean
 cd /usr/ports/security/rkhunter && make install clean
 cd /usr/ports/security/chkrootkit && make install clean
 cd /usr/ports/sysutils/topgrade && make install clean
+cd /usr/ports/textproc/bat && make install clean
 cd /usr/ports/ports-mgmt/portmaster && make install clean
 
 clear
