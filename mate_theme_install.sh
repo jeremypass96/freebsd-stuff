4d0fa987a707370e33bebf1e7db0eca7db109e93
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-12-13 15:17:56 -0500

    Update mate_theme_install.sh

--- mate_theme_install.sh
+++ mate_theme_install.sh
@@ -7,32 +7,6 @@ fi
 
 clear
 
-# Get wallpapers!
-echo "Getting wallpapers..."
-cd /usr/local/share/backgrounds
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0004.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0011.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0023.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0036.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0037.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0042.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0057.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0058.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0065.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0076.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0188.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0230.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0252.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0256.jpg
-sudo fetch https://gitlab.com/dwt1/wallpapers/-/raw/master/0257.jpg
-sudo fetch https://raw.githubusercontent.com/ghostbsd/ghostbsd-wallpapers/master/Field_Of_Lightning.jpg
-sudo fetch https://raw.githubusercontent.com/ghostbsd/ghostbsd-wallpapers/master/Lake_View.jpg
-sudo fetch https://raw.githubusercontent.com/ghostbsd/ghostbsd-wallpapers/master/Mountain_View.jpg
-sudo fetch https://raw.githubusercontent.com/ghostbsd/ghostbsd-wallpapers/master/Wood_Trail.jpg
-cd
-
-clear
-
 # Set wallpaper.
 gsettings set org.mate.background picture-options zoom && gsettings set org.mate.background picture-filename /usr/local/share/backgrounds/Wood_Trail.jpg
 # Set window titlebar font.
