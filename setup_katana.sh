7d6a0f351692769958b2d2376fc937435e4bde7e
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-02-10 02:48:53 -0500

    Update setup_katana.sh

--- setup_katana.sh
+++ setup_katana.sh
@@ -83,8 +83,9 @@ clear
 ./rcconf_setup.sh
 
 # Install cursor theme.
-echo "Installing the "Volantes Light Cursors" cursor theme..."
-tar -xvf volantes_light_cursors.tar.gz -C /usr/local/share/icons
+echo "Installing the "Bibata Modern Ice" cursor theme..."
+fetch https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.3/Bibata-Modern-Ice.tar.gz -o /home/$USER/Bibata-Modern-Ice.tar.gz
+tar -xvf /home/$USER/Bibata-Modern-Ice.tar.gz -C /usr/local/share/icons
 
 # Enable KDM (KDE4 display manager) on boot.
 sysrc kdm_enable="YES"
