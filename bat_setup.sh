9bd989370353158f7088333ea8f37ad14e99638b
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-08 18:21:58 -0400

    Update bat_setup.sh

--- bat_setup.sh
+++ bat_setup.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 # This script will setup the Catppuccin "mocha" theme for bat. FreeBSD version.
 
-cd && git clone https://github.com/catppuccin/bat.git
+# Clone and install the theme for the user
+cd "$HOME" || exit
+git clone https://github.com/catppuccin/bat.git
 cd bat
 mkdir -p "$(bat --config-dir)/themes"
 cp *.tmTheme "$(bat --config-dir)/themes"
 bat cache --build
-sed -i '' s/'1337/Catppuccin-mocha'/g /home/$USER/.config/bat/config
+sed -i '' 's/1337/Catppuccin-mocha/g' "$HOME/.config/bat/config"
+
+# Install the theme for root as well
+sudo sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp /usr/home/$SUDO_USER/bat/*.tmTheme "$(bat --config-dir)/themes"; bat cache --build; sed -i "" "s/1337/Catppuccin-mocha/g" "/root/.config/bat/config"'
+
+echo "Catppuccin 'mocha' theme for bat has been installed for both your user and root."
+rm -rf $HOME/bat
