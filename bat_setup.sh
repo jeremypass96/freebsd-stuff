66935c4be0b1dbf551588a4dcdfe52f888341c60
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-26 13:46:04 -0400

    Update bat_setup.sh

--- bat_setup.sh
+++ bat_setup.sh
@@ -1,18 +1,68 @@
 #!/bin/sh
 # This script will setup the Catppuccin "mocha" theme for bat. FreeBSD version.
 
-# Clone and install the theme for the user
+# Prompt the user to choose a theme
+echo "Select a theme for the 'bat' syntax highlighter:"
+echo "1. Latte"
+echo "2. Frappé"
+echo "3. Macchiato"
+echo "4. Mocha"
+read -p "Enter the number of your choice: " theme_choice
+
+case $theme_choice in
+    1)
+        selected_theme="Catppuccin-latte"
+        ;;
+    2)
+        selected_theme="Catppuccin-frappe"
+        ;;
+    3)
+        selected_theme="Catppuccin-macchiato"
+        ;;
+    4)
+        selected_theme="Catppuccin-mocha"
+        ;;
+    *)
+        echo "Invalid choice. Defaulting to 'Mocha' theme."
+        selected_theme="Catppuccin-mocha"
+        ;;
+esac
+
+# Generate initial configuration file for bat
+bat --generate-config-file
+
+# Modify the configuration settings for current user.
+sed -i "s/#--theme=\"TwoDark\"/--theme=\"$selected_theme\"/g" "$HOME/.config/bat/config"
+sed -i 's/#--italic-text=always/--italic-text=always/g' "$HOME/.config/bat/config"
+echo '--map-syntax "*.conf:INI"' >> "$HOME/.config/bat/config"
+echo '--map-syntax "config:INI"' >> "$HOME/.config/bat/config"
+
+# Copy the user configuration to /usr/share/skel so new users get the same setup.
+sudo mkdir -p /usr/share/skel/dot.config/bat
+sudo cp -v "$HOME/.config/bat/config" /usr/share/skel/dot.config/bat
+
+# Copy the user configuration to root's configuration.
+sudo mkdir -p /root/.config/bat
+sudo cp -v "$HOME/.config/bat/config" /root/.config/bat/
+
+# Setup the Catppuccin theme for bat.
 cd "$HOME" || exit
 git clone https://github.com/catppuccin/bat.git
 cd bat
-mkdir -p "$(bat --config-dir)/themes"
-cp *.tmTheme "$(bat --config-dir)/themes"
-bat cache --build
-sed -i '' 's/1337/Catppuccin-mocha/g' "$HOME/.config/bat/config"
+sudo sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp *.tmTheme "$(bat --config-dir)/themes"; bat cache --build'
 
-# Install the theme for root as well
-sudo sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp /usr/home/$SUDO_USER/bat/*.tmTheme "$(bat --config-dir)/themes"; bat cache --build; sed -i "" "s/1337/Catppuccin-mocha/g" "/root/.config/bat/config"'
+# Copy themes to /etc/skel.
+sudo sh -c 'mkdir -p /usr/share/skel/dot.config/bat/themes; cp *.tmTheme /usr/share/skel/dot.config/bat/themes; bat cache --build'
 
-echo "Bat syntax highlighter has been configured with the Catppuccin 'mocha' theme for both your user and root."
-rm -rf $HOME/bat
-sudo rm -rf root/bat
+# Modify the configuration settings for root.
+sed -i "s/#--theme=\"TwoDark\"/--theme=\"$selected_theme\"/g" "/root/.config/bat/config"
+sed -i 's/#--italic-text=always/--italic-text=always/g' "/root/.config/bat/config"
+echo '--map-syntax "*.conf:INI"' >> "/root/.config/bat/config"
+echo '--map-syntax "config:INI"' >> "/root/.config/bat/config"
+
+# Copy themes to root's home directory.
+sudo sh -c 'mkdir -p /root/.config/bat/themes; cp *.tmTheme /root/.config/bat/themes; bat cache --build'
+
+echo "Bat syntax highlighter has been configured with the selected theme ($selected_theme) for both your user and root."
+rm -rf "$HOME/bat"
+sudo rm -rf /root/bat
