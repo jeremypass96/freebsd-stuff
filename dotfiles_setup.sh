6cd8a85cb69a0208fd76fe4aefa23f9aac10c293
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-09-24 15:47:15 -0400

    Added bat config to script. A seperate shell script is pointless.

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -110,3 +110,73 @@ mkdir -p /usr/share/skel/dot.config/btop
 cp -v /home/$USER/freebsd-stuff/Dotfiles/config/btop/btop.conf /usr/share/skel/dot.config/btop/btop.conf
 mkdir -p /root/.config/btop
 cp -v /home/$USER/freebsd-stuff/Dotfiles/config/btop/btop.conf /root/.config/btop/btop.conf
+
+# Configure bat, a nicer and prettier cat clone.
+##
+# Prompt the user to choose a theme
+echo "Select a theme for the 'bat' syntax highlighter:"
+echo "1.) Latte"
+echo "2.) Frappé"
+echo "3.) Macchiato"
+echo "4.) Mocha"
+read -p "Enter the number of your choice: " theme_choice
+
+case $theme_choice in
+    1)
+        selected_theme="Catppuccin-latte"
+        ;;
+    2)
+        selected_theme="Catppuccin-frappe"
+        ;;
+    3)
+        selected_theme="Catppuccin-macchiato"
+        ;;
+    4)
+        selected_theme="Catppuccin-mocha"
+        ;;
+    *)
+        echo "Invalid choice. Defaulting to 'Mocha' theme."
+        selected_theme="Catppuccin-mocha"
+        ;;
+esac
+
+# Generate initial configuration file for bat (this script is running as root, remember?)
+bat --generate-config-file
+
+# Modify the configuration settings for the root user.
+sed -i '' 's/#--theme="TwoDark"/--theme="'"$selected_theme"'"'/g "$HOME/.config/bat/config"
+sed -i '' 's/#--italic-text=always/--italic-text=always'/g $HOME/.config/bat/config
+echo '--map-syntax "*.conf:INI"' >> $HOME/.config/bat/config
+echo '--map-syntax "config:INI"' >> $HOME/.config/bat/config
+
+# Copy the user configuration to /usr/share/skel so new users get the same setup.
+mkdir -p /usr/share/skel/dot.config/bat
+cp -v $HOME/.config/bat/config /usr/share/skel/dot.config/bat
+
+# Copy root's configuration to the user's configuration.
+mkdir -p /home/$USER/.config/bat
+cp -v $HOME/.config/bat/config /home/$USER/.config/bat/config
+
+# Setup the Catppuccin theme for bat.
+cd $HOME
+git clone https://github.com/catppuccin/bat.git
+cd bat
+sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp *.tmTheme "$(bat --config-dir)/themes"; bat cache --build'
+
+# Copy themes to /etc/skel.
+sh -c 'mkdir -p /usr/share/skel/dot.config/bat/themes; cp *.tmTheme /usr/share/skel/dot.config/bat/themes'
+
+# Modify the configuration settings for the user.
+sed -i -E 's/#--theme="TwoDark"/--theme="'"$selected_theme"'"'/g "/home/$USER/.config/bat/config"
+sed -i -E 's/#--italic-text=always/--italic-text=always'/g "/home/$USER/.config/bat/config"
+echo '--map-syntax "*.conf:INI"' >> /home/$USER/.config/bat/config
+echo '--map-syntax "config:INI"' >> /home/$USER/.config/bat/config
+chown -R $USER:$USER /home/$USER/.config/bat
+
+# Copy themes to user's home directory.
+sh -c 'mkdir -p /home/$USER/.config/bat/themes; cp *.tmTheme /home/$USER/.config/bat/themes; sudo -u $USER bat cache --build'
+chown -R $USER:$USER /home/$USER/.config/bat/themes
+
+echo "Bat syntax highlighter has been configured with the selected theme ($selected_theme) for both your user and root."
+rm -rf $HOME/bat
+##
