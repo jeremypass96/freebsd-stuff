13810d8ef679c19d28bd68e3f76a312e8d17c24a
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-10-15 22:15:00 -0400

    Slight fix.

--- setup_mate.sh
+++ setup_mate.sh
@@ -80,6 +80,7 @@ fi
 
 # Install packages.
 pkg install -y bash sudo xorg-minimal xorg-drivers xorg-fonts xorg-libraries noto-basic noto-emoji mate parole xfburn qt5ct qt5-style-plugins ulauncher ungoogled-chromium webfonts micro xclip zsh ohmyzsh neofetch pfetch octopkg lightdm slick-greeter mp4v2 skeuos-gtk-themes papirus-icon-theme numlockx devcpu-data automount fusefs-simple-mtpfs unix2dos smartmontools ubuntu-font webfonts droid-fonts-ttf materialdesign-ttf roboto-fonts-ttf plex-ttf xdg-user-dirs duf btop colorize freedesktop-sound-theme rkhunter chkrootkit topgrade bat fd-find lsd nerd-fonts
+
 clear
 
 # Setup rc.conf file.
