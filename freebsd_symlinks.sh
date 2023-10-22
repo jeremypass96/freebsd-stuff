fea2859818b18f36079e435790473cebae30b3c7
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-10-22 10:19:03 -0400

    Cleaned up.

--- freebsd_symlinks.sh
+++ freebsd_symlinks.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Checking to see if we're running as root.
+# Check if the script is running as root.
 if [ $(id -u) -ne 0 ]; then
-echo "Please run this script as root via 'su'! Thanks."
-exit
+  echo "Please run this script as root using 'su' or 'sudo'. Thanks."
+  exit 1
 fi
 
-# Symlink bash executable to /bin/bash so that scripts written on/for Linux can run properly.
-echo "Making a bash symlink to /bin/bash for Linux shell script
-compatibility..."
+# Symlink the bash executable to /bin/bash for Linux shell script compatibility.
+echo "Creating a symlink to /bin/bash for Linux shell script compatibility..."
 ln -sv /usr/local/bin/bash /bin/bash
 
-# Symlink themes/icons directories to enable installation of Linux themes.
-echo "Symlinking "/usr/local/share/themes" and "/usr/local/share/icons" to /usr/share/ for easy Linux themes and icons installation..."
-ln -sv /usr/local/share/themes /usr/share && ln -sv /usr/local/share/icons /usr/share
+# Symlink theme and icon directories for Linux themes and icons installation.
+echo "Symlinking theme and icon directories for easy Linux themes and icons installation..."
+ln -sv /usr/local/share/themes /usr/share
+ln -sv /usr/local/share/icons /usr/share
