186819efd6f663589a475a012166df073d5b0d57
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2025-06-12 19:45:43 -0400

    Update setup_mate.sh

--- setup_mate.sh
+++ setup_mate.sh
@@ -132,7 +132,6 @@ if [ $resp -eq 0 ]; then
 else
     dialog --title "BSDstats Skipped" --infobox "Skipping BSDstats installation." 5 30
     sleep 2
-    fi
 fi
 
 if [ "$resp" = ports ]; then
@@ -355,7 +354,6 @@ if [ $resp -eq 0 ]; then
 else
     dialog --title "BSDstats Skipped" --infobox "Skipping BSDstats installation." 5 30
     sleep 2
-    fi
 fi
 
 clear
